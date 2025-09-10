e5e1eef41527eb45947ba1d42d4f41f031e66d78
0xAustinWang <austiwang+eth@gmail.com>
2025-09-10 12:59:35 -0300

    bring back race condition fixes

--- executor/executor_coordinator.go
+++ executor/executor_coordinator.go
@@ -5,11 +5,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"time"
-
 	"github.com/smartcontractkit/chainlink-ccv/executor/pkg/utils"
 	"github.com/smartcontractkit/chainlink-ccv/executor/types"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+	"sync"
+	"time"
 
 	cdr "github.com/smartcontractkit/chainlink-ccv/executor/pkg/ccvdatareader"
 	e "github.com/smartcontractkit/chainlink-ccv/executor/pkg/executor"
@@ -27,6 +27,7 @@ type Coordinator struct {
 	cancel              context.CancelFunc
 	running             bool
 	delayedMessageHeap  *utils.MessageHeap
+	mu                  sync.RWMutex
 }
 
 type Option func(*Coordinator)
@@ -83,6 +84,8 @@ func NewCoordinator(options ...Option) (*Coordinator, error) {
 }
 
 func (ec *Coordinator) Start(ctx context.Context) error {
+	ec.mu.Lock()
+	defer ec.mu.Unlock()
 	if ec.running {
 		return fmt.Errorf("Coordinator already running")
 	}
@@ -101,9 +104,12 @@ func (ec *Coordinator) Start(ctx context.Context) error {
 }
 
 func (ec *Coordinator) Stop() error {
+	ec.mu.RLock()
 	if !ec.running {
+		ec.mu.RUnlock()
 		return fmt.Errorf("Coordinator not started")
 	}
+	ec.mu.RUnlock()
 
 	ec.lggr.Infow("Coordinator stopping")
 	ec.cancel()
@@ -117,6 +123,8 @@ func (ec *Coordinator) run(ctx context.Context) {
 	defer close(ec.doneCh)
 	defer func() {
 		ec.lggr.Infow("Coordinator run loop exited")
+		ec.mu.Lock()
+		defer ec.mu.Unlock()
 		ec.running = false
 	}()
 
@@ -178,5 +186,7 @@ func (ec *Coordinator) run(ctx context.Context) {
 
 // IsRunning returns whether the coordinator is running.
 func (ec *Coordinator) IsRunning() bool {
+	ec.mu.RLock()
+	defer ec.mu.RUnlock()
 	return ec.running
 }
