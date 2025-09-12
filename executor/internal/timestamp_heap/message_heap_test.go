9cc98f05475a2c7555b9ede14c2bcee8450a9b15
0xAustinWang <austiwang+eth@gmail.com>
2025-09-12 10:15:49 -0300

    fix pointer bytes

--- executor/internal/timestamp_heap/message_heap_test.go
+++ executor/internal/timestamp_heap/message_heap_test.go
@@ -32,8 +32,8 @@ func createMessageWithTimestamp(readyTime int64, seqNum uint64) *MessageWithTime
 
 func TestMessageHeap_PeekTime(t *testing.T) {
 	tests := []struct {
-		heap     MessageHeap
 		name     string
+		heap     MessageHeap
 		expected int64
 	}{
 		{
@@ -68,11 +68,11 @@ func TestMessageHeap_PeekTime(t *testing.T) {
 
 func TestMessageHeap_PopAllReady(t *testing.T) {
 	tests := []struct {
-		heap            MessageHeap
 		name            string
+		heap            MessageHeap
+		expectedSeqNums []uint64
 		timestamp       int64
 		expectedCount   int
-		expectedSeqNums []uint64
 		remainingCount  int
 	}{
 		{
