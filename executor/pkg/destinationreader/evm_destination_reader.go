6d47f76578f3acbf9718853ce90f8fbb41130f68
0xAustinWang <austiwang+eth@gmail.com>
2025-09-15 17:57:51 -0300

    destination reader

--- executor/pkg/destinationreader/evm_destination_reader.go
+++ executor/pkg/destinationreader/evm_destination_reader.go
@@ -2,6 +2,8 @@ package destinationreader
 
 import (
 	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 
@@ -9,6 +11,7 @@ import (
 	"github.com/smartcontractkit/chainlink-ccv/executor/types"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 
+	mockReceiver "github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/latest/mock_receiver_v2"
 	commontypes "github.com/smartcontractkit/chainlink-ccv/common/pkg/types"
 	protocol "github.com/smartcontractkit/chainlink-ccv/protocol/pkg/types"
 )
@@ -33,39 +36,32 @@ func NewEvmDestinationReaderFromChainInfo(ctx context.Context, lggr logger.Logge
 // GetCCVSForMessage implements the DestinationReader interface. It uses the chainlink-evm client to call the get_ccvs function on the receiver contract.
 // The ABI is defined here https://github.com/smartcontractkit/chainlink-ccip/blob/0e7fcfd20ab005d75d0eb863790470f91fa5b8d7/chains/evm/contracts/interfaces/IAny2EVMMessageReceiverV2.sol
 func (dr *EvmDestinationReader) GetCCVSForMessage(ctx context.Context, sourceSelector protocol.ChainSelector, receiverAddress protocol.UnknownAddress) (types.CcvAddressInfo, error) {
-	offRamp, _ := protocol.NewUnknownAddressFromHex("0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1")
+	receiverContract, err := mockReceiver.NewMockReceiverV2Caller(common.HexToAddress(receiverAddress.String()), dr.client)
+	if err != nil {
+		return types.CcvAddressInfo{}, fmt.Errorf("failed to create receiver contract instance: %w", err)
+	}
+
+	req, opt, optThreshold, err := receiverContract.GetCCVs(nil, uint64(sourceSelector))
+	if err != nil {
+		return types.CcvAddressInfo{}, fmt.Errorf("failed to call getCCVs: %w", err)
+	}
+
+	// Convert common.Address slices to protocol.UnknownAddress slices
+	requiredCCVs := make([]protocol.UnknownAddress, len(req))
+	for i, addr := range req {
+		requiredCCVs[i] = protocol.UnknownAddress(addr.Hex())
+	}
+
+	optionalCCVs := make([]protocol.UnknownAddress, len(opt))
+	for i, addr := range opt {
+		optionalCCVs[i] = protocol.UnknownAddress(addr.Hex())
+	}
+
 	return types.CcvAddressInfo{
-		RequiredCcvs:      []protocol.UnknownAddress{offRamp},
-		OptionalCcvs:      []protocol.UnknownAddress{},
-		OptionalThreshold: 0,
+		RequiredCcvs:      requiredCCVs,
+		OptionalCcvs:      optionalCCVs,
+		OptionalThreshold: optThreshold,
 	}, nil
-	//
-	//receiverContract, err := rcv.NewEtherSenderReceiverCaller(common.HexToAddress(receiverAddress.String()), dr.client)
-	//if err != nil {
-	//	return types.CcvAddressInfo{}, fmt.Errorf("failed to create receiver contract instance: %w", err)
-	//}
-	//
-	//result, err := receiverContract.GetCCVs(nil, uint64(sourceSelector))
-	//if err != nil {
-	//	return types.CcvAddressInfo{}, fmt.Errorf("failed to call getCCVs: %w", err)
-	//}
-	//
-	//// Convert common.Address slices to protocol.UnknownAddress slices
-	//requiredCCVs := make([]protocol.UnknownAddress, len(result.RequiredCCVs))
-	//for i, addr := range result.RequiredCCVs {
-	//	requiredCCVs[i] = protocol.UnknownAddress(addr.Hex())
-	//}
-	//
-	//optionalCCVs := make([]protocol.UnknownAddress, len(result.OptionalCCVs))
-	//for i, addr := range result.OptionalCCVs {
-	//	optionalCCVs[i] = protocol.UnknownAddress(addr.Hex())
-	//}
-	//
-	//return types.CcvAddressInfo{
-	//	RequiredCcvs:      requiredCCVs,
-	//	OptionalCcvs:      optionalCCVs,
-	//	OptionalThreshold: result.OptionalThreshold,
-	//}, nil
 }
 
 // IsMessageExecuted checks the destination chain to verify if a message has been executed
