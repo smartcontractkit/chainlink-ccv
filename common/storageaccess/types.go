3508cd05707ab2ae9224903497a64003c01cec37
Kodey Thomas <kodey.kildaythomas@smartcontract.com>
2025-09-16 12:07:00 -0300

    feat: indexer adapter (#67)
    * feat: indexer adapter
    
    * chore: format
    
    * chore: linting
    
    * chore: close response body
    
    * chore: lint
    
    * chore: lint
    
    * chore: naming
    
    * chore: struct
    
    * chore: struct
    
    * chore: help
    
    * chore: pointer?
    
    * chore: ignore govet warning
    
    * chore: ignore govet warning

---
+++ common/storageaccess/types.go
@@ -0,0 +1,27 @@
+package storageaccess
+
+import (
+	"context"
+
+	"github.com/smartcontractkit/chainlink-ccv/protocol/pkg/types"
+)
+
+type IndexerAPI interface {
+	// ReadVerifierResults reads all data that matches the provided query parameters. Returns a map of messageID to an array of all known VerifierResults for that messageID. Does not provide a guarantee that the returned data is enough to be executed.
+	ReadVerifierResults(ctx context.Context, queryData VerifierResultsRequest) (map[string][]types.CCVData, error)
+}
+
+type VerifierResultsRequest struct { //nolint:govet
+	Limit                uint64                `form:"limit"`
+	Offset               uint64                `form:"offset"`
+	Start                int64                 `form:"start"`
+	End                  int64                 `form:"end"`
+	SourceChainSelectors []types.ChainSelector // Excluded from form due to gin parsing
+	DestChainSelectors   []types.ChainSelector // Excluded from form due to gin parsing
+}
+
+type VerifierResultsResponse struct {
+	CCVData map[string][]types.CCVData `json:"ccvData"`
+	Error   string                     `json:"error,omitempty"`
+	Success bool                       `json:"success"`
+}
