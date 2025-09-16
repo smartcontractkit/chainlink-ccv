59bde55d8f04be4ba78d1111968d1aab1866ccd0
Kodey Kilday-Thomas <kodey.kildaythomas@smartcontract.com>
2025-09-16 11:42:30 -0300

    chore: pointer?

--- common/storageaccess/types.go
+++ common/storageaccess/types.go
@@ -12,11 +12,10 @@ type IndexerAPI interface {
 }
 
 type VerifierResultsRequest struct {
-	Start  int64  `form:"start"`
-	End    int64  `form:"end"`
-	Limit  uint64 `form:"limit"`
-	Offset uint64 `form:"offset"`
-
+	Limit                uint64                `form:"limit"`
+	Offset               uint64                `form:"offset"`
+	Start                int64                 `form:"start"`
+	End                  int64                 `form:"end"`
 	SourceChainSelectors []types.ChainSelector // Excluded from form due to gin parsing
 	DestChainSelectors   []types.ChainSelector // Excluded from form due to gin parsing
 }
