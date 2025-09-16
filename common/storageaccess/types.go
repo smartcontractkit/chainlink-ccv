306f851053aa518871bf11c7214075be82e245f1
Kodey Kilday-Thomas <kodey.kildaythomas@smartcontract.com>
2025-09-16 11:14:02 -0300

    chore: lint

--- common/storageaccess/types.go
+++ common/storageaccess/types.go
@@ -7,6 +7,6 @@ import (
 )
 
 type IndexerAPI interface {
-	// Reads all CCVData that matches the provided query parameters. Returns a map of messageID to an array of all known CCVData for that messageID. Does not provide a guarantee that the returned CCVData is enough to be executed.
+	// ReadCCVData reads all data that matches the provided query parameters. Returns a map of messageID to an array of all known CCVData for that messageID. Does not provide a guarantee that the returned CCVData is enough to be executed.
 	ReadCCVData(ctx context.Context, startUnix, endUnix int64, sourceChainSelectors, destChainSelectors []types.ChainSelector, limit, offset uint64) (map[string][]types.CCVData, error)
 }
