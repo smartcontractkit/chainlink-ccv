8baa70203433fa7fbdd36665779869d24ad7dfea
Aleksandr Bukata <aleksandr.bukata@smartcontract.com>
2025-09-17 18:19:19 -0300

    feature: scan events outside of protocol and publish as traces to tempo

--- build/devenv/monitoring.go
+++ build/devenv/monitoring.go
@@ -2,13 +2,22 @@ package ccv
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
+	"strconv"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	chain_selectors "github.com/smartcontractkit/chain-selectors"
+	"go.uber.org/zap"
 
 	ccvAggregator "github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/latest/ccv_aggregator"
 	ccvProxy "github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/latest/ccv_proxy"
+	"github.com/smartcontractkit/chainlink-ccv/common/storageaccess"
+	"github.com/smartcontractkit/chainlink-ccv/protocol/pkg/types"
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 )
 
 const (
@@ -32,13 +41,24 @@ var msgSentTotal = promauto.NewCounter(prometheus.CounterOpts{
 // in case we need to get realtime metrics this function can be converted to a service that calls this function with
 // block ranges for both chains.
 func MonitorOnChainLogs(in *Cfg) error {
+	Plog.Info().Str("AggregatorAddress", in.Verifier.AggregatorAddress).Send()
 	bcs, err := blockchainsByChainID(in)
 	if err != nil {
 		return err
 	}
 	lp := NewLokiPusher()
-	ctx := context.Background()
+	tp := NewTempoPusher()
+	lggr, err := logger.NewWith(func(config *zap.Config) {
+		config.Development = true
+		config.Encoding = "console"
+		config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	})
 
+	storageReader, err := storageaccess.NewAggregatorReader("localhost:50051", lggr, 0)
+	if err != nil {
+		lggr.Errorw("Failed to create storage reader", "error", err)
+	}
+	ctx := context.Background()
 	msgSentStreams, err := FilterContractEventsAllChains[ccvProxy.CCVProxyCCIPMessageSent](
 		ctx,
 		in,
@@ -52,7 +72,7 @@ func MonitorOnChainLogs(in *Cfg) error {
 	if err != nil {
 		return fmt.Errorf("failed to collect logs: %w", err)
 	}
-	executionStateChangedStreams, err := FilterContractEventsAllChains[ccvAggregator.CCVAggregatorExecutionStateChanged](
+	msgExecStreams, err := FilterContractEventsAllChains[ccvAggregator.CCVAggregatorExecutionStateChanged](
 		ctx,
 		in,
 		bcs,
@@ -62,6 +82,7 @@ func MonitorOnChainLogs(in *Cfg) error {
 		nil,
 		nil,
 	)
+	ccv, err := storageReader.ReadCCVData(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to collect logs: %w", err)
 	}
@@ -69,35 +90,136 @@ func MonitorOnChainLogs(in *Cfg) error {
 	if err := lp.PushRawAndDecoded(msgSentStreams.RawLoki, msgSentStreams.DecodedLoki, "on-chain"); err != nil {
 		return fmt.Errorf("failed to push logs: %w", err)
 	}
-	if err := lp.PushRawAndDecoded(executionStateChangedStreams.RawLoki, executionStateChangedStreams.DecodedLoki, "on-chain"); err != nil {
+	if err := lp.PushRawAndDecoded(msgExecStreams.RawLoki, msgExecStreams.DecodedLoki, "on-chain"); err != nil {
 		return fmt.Errorf("failed to push logs: %w", err)
 	}
 	// process Prom metrics
-	logTimeByMsgID := make(map[[32]byte]uint64)
+	msgSentEvents := make(map[types.Bytes32]*DecodedLog[ccvProxy.CCVProxyCCIPMessageSent])
+	msgSigEvents := make(map[types.Bytes32]types.CCVData)
+	msgExecEvents := make(map[types.Bytes32]*DecodedLog[ccvAggregator.CCVAggregatorExecutionStateChanged])
+	logTimeByMsgID := make(map[types.Bytes32]uint64)
 	for _, l := range msgSentStreams.DecodedProm {
-		if l.ChainID == 1337 && l.Name == "CCIPMessageSent" {
+		if l.Name == "CCIPMessageSent" {
 			if payload, ok := any(l.UnpackedData).(ccvProxy.CCVProxyCCIPMessageSent); ok {
+				payload.DestChainSelector = binary.BigEndian.Uint64(l.Topics[1][24:]) // Last 8 bytes
+				payload.SequenceNumber = binary.BigEndian.Uint64(l.Topics[2][24:])    // Last 8 bytes
+				copy(payload.MessageId[:], l.Topics[3][:])                            // Full 32 bytes
 				Plog.Info().
 					Str("MsgID", fmt.Sprintf("%x", payload.MessageId)).
 					Uint64("BlockTimestamp", l.BlockTimestamp).
 					Msg("Received CCIPMessageSent log")
 				msgSentTotal.Inc()
 				logTimeByMsgID[payload.MessageId] = l.BlockTimestamp
+				msgSentEvents[payload.MessageId] = l
 			}
 		}
 	}
-	for _, l := range executionStateChangedStreams.DecodedProm {
-		if l.ChainID == 2337 && l.Name == "ExecutionStateChanged" {
+	for _, l := range msgExecStreams.DecodedProm {
+		if l.Name == "ExecutionStateChanged" {
 			if payload, ok := any(l.UnpackedData).(ccvAggregator.CCVAggregatorExecutionStateChanged); ok {
+				payload.SourceChainSelector = binary.BigEndian.Uint64(l.Topics[1][24:]) // Last 8 bytes
+				payload.SequenceNumber = binary.BigEndian.Uint64(l.Topics[2][24:])      // Last 8 bytes
+				copy(payload.MessageId[:], l.Topics[3][:])                              // Full 32 bytes
 				Plog.Info().
 					Str("MsgID", fmt.Sprintf("%x", payload.MessageId)).
 					Uint64("BlockTimestamp", l.BlockTimestamp).
 					Msg("Received ExecutionStateChanged log")
-				// srcDstLatency.Observe(float64(l.BlockTimestamp))
+				msgExecEvents[payload.MessageId] = l
+				// TODO: get msgID and measure time for each variant of messages, no events emitted yet
+				//srcDstLatency.Observe(float64(l.BlockTimestamp))
 			}
+		}
+	}
+	for _, l := range ccv {
+		msgSigEvents[l.Data.MessageID] = l.Data
+		Plog.Info().
+			Str("MsgID", l.Data.MessageID.String()).
+			Int64("Timestamp", l.Data.Timestamp).
+			Msg("Received verifier signature")
+	}
+	for msgId, msgSent := range msgSentEvents {
+		msgSig, okSig := msgSigEvents[msgId]
+		_, okExec := msgExecEvents[msgId]
+		if !okSig && !okExec {
+			Plog.Warn().Str("msg", "No corresponding sig and exec found").Send()
+			continue
+		}
+		spans := make([]Span, 0, 2)
+		traceId := randHex(16)
+		rootSpan := randHex(8)
+		evmChain, _ := chain_selectors.GetChainDetailsByChainIDAndFamily(strconv.FormatInt(msgSent.ChainID, 10), chain_selectors.FamilyEVM)
+		// TODO don't mock data
+		spans = append(spans, Span{
+			TraceId:           traceId,
+			SpanId:            rootSpan,
+			Name:              "msg_exec",
+			StartTimeUnixNano: msgSent.BlockTimestamp * 1_000_000_000,
+			EndTimeUnixNano:   (uint64(msgSig.Timestamp) + (uint64(msgSig.Timestamp) - msgSent.BlockTimestamp)) * 1_000_000_000,
+			Kind:              2,
+			Attributes: []Attribute{
+				{
+					Key: "sourceChainSelector",
+					Value: map[string]any{
+						"stringValue": strconv.FormatUint(evmChain.ChainSelector, 10),
+					},
+				},
+				{
+					Key: "destChainSelector",
+					Value: map[string]any{
+						"stringValue": strconv.FormatUint(msgSent.UnpackedData.DestChainSelector, 10),
+					},
+				},
+				{
+					Key: "messageId",
+					Value: map[string]any{
+						"stringValue": msgId.String(),
+					},
+				},
+			},
+		})
+
+		if okSig {
+			spans = append(spans, Span{
+				TraceId:           traceId,
+				ParentSpanId:      rootSpan,
+				SpanId:            randHex(8),
+				Name:              "msg_sig",
+				StartTimeUnixNano: msgSent.BlockTimestamp * 1_000_000_000,
+				EndTimeUnixNano:   uint64(msgSig.Timestamp) * 1_000_000_000,
+				Kind:              2,
+				Attributes: []Attribute{
+					{
+						Key: "sourceChainSelector",
+						Value: map[string]any{
+							"stringValue": msgSig.SourceChainSelector.String(),
+						},
+					},
+					{
+						Key: "destChainSelector",
+						Value: map[string]any{
+							"stringValue": msgSig.DestChainSelector.String(),
+						},
+					},
+					{
+						Key: "messageId",
+						Value: map[string]any{
+							"stringValue": msgId.String(),
+						},
+					},
+				},
+			})
+		}
+		if err := tp.PushTrace(spans); err != nil {
+			return fmt.Errorf("failed to push traces: %w", err)
 		}
 	}
 	Plog.Info().Str("LokiStreamURL", LokiOnChainStreamURL).Send()
 	Plog.Info().Str("PrometheusMetrics", PromMetrics).Send()
 	return nil
+}
+
+func randHex(nBytes int) string {
+	b := make([]byte, nBytes)
+	_, _ = rand.Read(b)
+	return hex.EncodeToString(b) // lowercase, no hyphens
 }
---
+++ build/devenv/tempo.go
@@ -0,0 +1,117 @@
+package ccv
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
+
+type TempoPayload struct {
+	ResourceSpans []ResourceSpan `json:"resourceSpans"`
+}
+
+type ResourceSpan struct {
+	Resource   Scope       `json:"resource"`
+	ScopeSpans []ScopeSpan `json:"scopeSpans"`
+}
+
+type ScopeSpan struct {
+	Scope Scope  `json:"scope"`
+	Spans []Span `json:"spans"`
+}
+
+type Scope struct {
+	Name       string      `json:"name"`
+	Version    string      `json:"version"`
+	Attributes []Attribute `json:"attributes"`
+}
+
+type Span struct {
+	TraceId           string      `json:"traceId"`
+	ParentSpanId      string      `json:"parentSpanId,omitempty"`
+	SpanId            string      `json:"spanId"`
+	Name              string      `json:"name"`
+	StartTimeUnixNano uint64      `json:"startTimeUnixNano"`
+	EndTimeUnixNano   uint64      `json:"endTimeUnixNano"`
+	Kind              uint8       `json:"kind"`
+	Attributes        []Attribute `json:"attributes"`
+}
+
+type Attribute struct {
+	Key   string         `json:"key"`
+	Value map[string]any `json:"value"`
+}
+
+// TempoPusher handles pushing traces to Tempo
+type TempoPusher struct {
+	tempoURL string
+	client   *resty.Client
+}
+
+// NewTempoPusher creates a new TempoPusher instance
+func NewTempoPusher() *TempoPusher {
+	tempoURL := os.Getenv("TEMPO_URL")
+	if tempoURL == "" {
+		tempoURL = DefaultTempoURL
+	}
+	return &TempoPusher{
+		tempoURL: tempoURL,
+		client:   resty.New().SetTimeout(10 * time.Second),
+	}
+}
+
+func (tp *TempoPusher) PushTrace(spans []Span) error {
+	Plog.Info().Msgf("Pushing spans to %v", tp.tempoURL)
+	payload := TempoPayload{
+		ResourceSpans: []ResourceSpan{
+			{
+				Resource: Scope{
+					Attributes: []Attribute{
+						{
+							Key: "service.name",
+							Value: map[string]any{
+								"stringValue": "on-chain",
+							},
+						},
+					},
+				},
+				ScopeSpans: []ScopeSpan{
+					{
+						Scope: Scope{
+							Name:    "name",
+							Version: "version",
+							Attributes: []Attribute{
+								{
+									Key: "name",
+									Value: map[string]any{
+										"stringValue": "on-chain",
+									},
+								},
+							},
+						},
+						Spans: spans,
+					},
+				},
+			},
+		},
+	}
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("failed to marshal payload: %w", err)
+	}
+	Plog.Info().Msgf("Payload: %v", string(jsonPayload))
+	resp, err := tp.client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(payload).
+		Post(tp.tempoURL)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+	if resp.StatusCode() != 200 && resp.StatusCode() != 204 {
+		return fmt.Errorf("tempo returned status %d: %s", resp.StatusCode(), resp.String())
+	}
+	return nil
+}
