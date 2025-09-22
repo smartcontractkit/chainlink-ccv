6b5ee7c2393505cc6fff22f2f2935f9f4cbfec3c
0xAustinWang <austiwang+eth@gmail.com>
2025-09-22 16:08:12 -0700

    add cache for querying CCVs on destination chain

--- executor/pkg/destinationreader/evm_destination_reader.go
+++ executor/pkg/destinationreader/evm_destination_reader.go
@@ -3,9 +3,11 @@ package destinationreader
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	lru "github.com/hashicorp/golang-lru/v2/expirable"
 
 	"github.com/smartcontractkit/chainlink-ccv/common/pkg"
 	"github.com/smartcontractkit/chainlink-ccv/executor/types"
@@ -24,34 +26,58 @@ const (
 	MESSAGE_FAILURE
 )
 
+// Ensure ChainlinkExecutor implements the Executor interface.
+var _ DestinationReader = &EvmDestinationReader{}
+
+type cacheKey struct {
+	sourceChainSelector protocol.ChainSelector
+	receiverAddress     string
+}
 type EvmDestinationReader struct {
 	aggregatorCaller ccvagg.CCVAggregatorCaller
 	lggr             logger.Logger
 	client           bind.ContractCaller
 	chainSelector    uint64
+	ccvCache         *lru.LRU[cacheKey, types.CcvAddressInfo]
 }
 
 func NewEvmDestinationReaderFromChainInfo(ctx context.Context, lggr logger.Logger, chainSelector uint64, chainInfo *commontypes.BlockchainInfo) *EvmDestinationReader {
 	chainClient := pkg.CreateMultiNodeClientFromInfo(ctx, chainInfo, lggr)
 
-	ccvAddr := common.HexToAddress(chainInfo.OfframpRouter)
+	ccvAddr := common.HexToAddress(chainInfo.OfframpAddress)
 	ccvAgg, err := ccvagg.NewCCVAggregatorCaller(ccvAddr, chainClient)
 	if err != nil {
 		lggr.Errorw("Failed to create CCV Aggregator caller", "error", err, "address", ccvAddr.Hex(), "chainSelector", chainSelector)
 	}
+	// Create cache with max 1000 entries and 5-minute TTL
+	ccvCache := lru.NewLRU[cacheKey, types.CcvAddressInfo](1000, nil, 5*time.Minute)
+
 	return &EvmDestinationReader{
 		aggregatorCaller: *ccvAgg,
 		lggr:             lggr,
 		chainSelector:    chainSelector,
 		client:           chainClient,
+		ccvCache:         ccvCache,
 	}
 }
 
 // GetCCVSForMessage implements the DestinationReader interface. It uses the chainlink-evm client to call the get_ccvs function on the receiver contract.
 // The ABI is defined here https://github.com/smartcontractkit/chainlink-ccip/blob/0e7fcfd20ab005d75d0eb863790470f91fa5b8d7/chains/evm/contracts/interfaces/IAny2EVMMessageReceiverV2.sol
-func (dr *EvmDestinationReader) GetCCVSForMessage(ctx context.Context, sourceSelector protocol.ChainSelector, receiverAddress protocol.UnknownAddress) (types.CcvAddressInfo, error) {
+func (dr *EvmDestinationReader) GetCCVSForMessage(ctx context.Context, message protocol.Message) (types.CcvAddressInfo, error) {
 	_ = ctx
-	receiverContract, err := mockreceiver.NewMockReceiverV2Caller(common.HexToAddress(receiverAddress.String()), dr.client)
+	receiverAddress, sourceSelector := message.Receiver, message.SourceChainSelector
+	evmReceiverAddress := common.BytesToAddress(receiverAddress)
+	// Try to get CCV info from cache first
+	// TODO: Do we need custom cache eviction logic beyond ttl?
+	ccvInfo, found := dr.ccvCache.Get(cacheKey{sourceChainSelector: sourceSelector, receiverAddress: string(receiverAddress)})
+	if found {
+		dr.lggr.Debugf("CCV info retrieved from cache for receiver %s on source chain %d",
+			string(receiverAddress), sourceSelector)
+		return ccvInfo, nil
+	}
+
+	// TODO: use the new function from the CCVAggregator / offramp
+	receiverContract, err := mockreceiver.NewMockReceiverV2Caller(evmReceiverAddress, dr.client)
 	if err != nil {
 		return types.CcvAddressInfo{}, fmt.Errorf("failed to create receiver contract instance: %w", err)
 	}
@@ -61,29 +87,35 @@ func (dr *EvmDestinationReader) GetCCVSForMessage(ctx context.Context, sourceSel
 		return types.CcvAddressInfo{}, fmt.Errorf("failed to call getCCVs: %w", err)
 	}
 
-	// Convert common.Address slices to protocol.UnknownAddress slices
-	requiredCCVs := make([]protocol.UnknownAddress, len(req))
-	for i, addr := range req {
-		requiredCCVs[i] = protocol.UnknownAddress(addr.Hex())
+	requiredCCVs := make([]protocol.UnknownAddress, 0)
+	optionalCCVs := make([]protocol.UnknownAddress, 0)
+	for _, addr := range req {
+		requiredCCVs = append(requiredCCVs, protocol.UnknownAddress(addr.Hex()))
 	}
 
-	optionalCCVs := make([]protocol.UnknownAddress, len(opt))
-	for i, addr := range opt {
-		optionalCCVs[i] = protocol.UnknownAddress(addr.Hex())
+	for _, addr := range opt {
+		optionalCCVs = append(optionalCCVs, protocol.UnknownAddress(addr.Hex()))
 	}
 
-	return types.CcvAddressInfo{
+	ccvInfo = types.CcvAddressInfo{
 		RequiredCcvs:      requiredCCVs,
 		OptionalCcvs:      optionalCCVs,
 		OptionalThreshold: optThreshold,
-	}, nil
+	}
+
+	// Store in cache for future use
+	dr.ccvCache.Add(cacheKey{sourceChainSelector: sourceSelector, receiverAddress: string(receiverAddress)}, ccvInfo)
+	dr.lggr.Debugf("CCV info cached for receiver %s on source chain %d",
+		string(receiverAddress), sourceSelector)
+
+	return ccvInfo, nil
 }
 
 // IsMessageExecuted checks the destination chain to verify if a message has been executed.
 func (dr *EvmDestinationReader) IsMessageExecuted(ctx context.Context, message protocol.Message) (bool, error) {
 	_ = ctx
 
-	rcv := common.HexToAddress(string(message.Receiver))
+	rcv := common.BytesToAddress(message.Receiver)
 	execState, err := dr.aggregatorCaller.GetExecutionState(
 		&bind.CallOpts{
 			Context: ctx,
