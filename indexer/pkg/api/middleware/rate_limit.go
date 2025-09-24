f7c937dfdce9c05497f3453b929b6cc4d93ff3a4
Kodey Kilday-Thomas <kodey.kildaythomas@smartcontract.com>
2025-09-24 13:40:02 +0100

    chore: use middleware

--- indexer/pkg/api/middleware/rate_limit.go
+++ indexer/pkg/api/middleware/rate_limit.go
@@ -1,12 +1,13 @@
 package middleware
 
 import (
-	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ulule/limiter/v3"
 	"github.com/ulule/limiter/v3/drivers/store/memory"
+
+	mgin "github.com/ulule/limiter/v3/drivers/middleware/gin"
 )
 
 var DefaultRateLimit = limiter.Rate{
@@ -18,21 +19,5 @@ func RateLimit() gin.HandlerFunc {
 	store := memory.NewStore()
 	instance := limiter.New(store, DefaultRateLimit)
 
-	return func(c *gin.Context) {
-		ip := c.ClientIP()
-		context, err := instance.Get(c, ip)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			c.Abort()
-			return
-		}
-
-		if context.Reached {
-			c.JSON(http.StatusTooManyRequests, gin.H{"error": "Rate limit exceeded"})
-			c.Abort()
-			return
-		}
-
-		c.Next()
-	}
+	return mgin.NewMiddleware(instance)
 }
