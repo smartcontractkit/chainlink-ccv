3ab972bb3ae02a76bfdd28b6c2468b2edae12b3d
Kodey Thomas <kodey.kildaythomas@smartcontract.com>
2025-10-06 10:59:13 +0100

    feat(indexer): messages endpoint (#152)
    * feat: messages endpoint
    
    * chore: linting
    
    * chore: linting
    
    * chore: linting
    
    * chore: linting
    
    * chore: pr comments

--- indexer/pkg/api/handlers/v1/types.go
+++ indexer/pkg/api/handlers/v1/types.go
@@ -6,3 +6,16 @@ type MessageIDV1Response struct {
 	MessageID     string             `json:"messageID"`
 	Verifications []protocol.CCVData `json:"verifications"`
 }
+
+type MessagesV1Request struct {
+	SourceChainSelectors []protocol.ChainSelector // Excluded from form due to gin parsing
+	DestChainSelectors   []protocol.ChainSelector // Excluded from form due to gin parsing
+	Start                int64                    `form:"start"`
+	End                  int64                    `form:"end"`
+	Limit                uint64                   `form:"limit"`
+	Offset               uint64                   `form:"offset"`
+}
+
+type MessagesV1Response struct {
+	Messages map[string]protocol.Message `json:"messages"`
+}
