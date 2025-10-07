3e87205453fe73ce1990fbb21a7f566aa676b721
Will Winder <wwinder.unh@gmail.com>
2025-10-07 12:51:52 -0400

    Do not use go-ethereum for hasher.

--- protocol/hashing.go
+++ protocol/hashing.go
@@ -1,11 +1,30 @@
 package protocol
 
-import "github.com/ethereum/go-ethereum/crypto"
+import (
+	"hash"
+	"sync"
+
+	"golang.org/x/crypto/sha3"
+)
+
+var hasherPool = sync.Pool{
+	New: func() any {
+		return sha3.NewLegacyKeccak256()
+	},
+}
 
 // Keccak256 computes the Keccak256 hash of the input.
 func Keccak256(data []byte) [32]byte {
-	hash := crypto.Keccak256(data)
-	var result [32]byte
-	copy(result[:], hash)
-	return result
+	h, ok := hasherPool.Get().(hash.Hash)
+	if !ok {
+		panic("cannot get hasher")
+	}
+
+	h.Reset()
+	h.Write(data) // nolint:revive // keccak256 never returns an error
+	var out [32]byte
+	copy(out[:], h.Sum(nil))
+	h.Reset()
+	hasherPool.Put(h)
+	return out
 }
