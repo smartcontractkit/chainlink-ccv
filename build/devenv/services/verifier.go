8a2ddc67c4a3cd46ff9e5be9625749eb14d3864a
Will Winder <wwinder.unh@gmail.com>
2025-10-10 17:06:17 -0400

    Update devenv and dockerfiles.

--- build/devenv/services/verifier.go
+++ build/devenv/services/verifier.go
@@ -180,7 +180,21 @@ func NewVerifier(in *VerifierInput) (*VerifierOutput, error) {
 
 	if in.SourceCodePath != "" {
 		req.Mounts = testcontainers.Mounts()
-		req.Mounts = append(req.Mounts, GoSourcePathMounts(p, in.RootPath, AppPathInsideContainer)...)
+
+		req.Mounts = append(req.Mounts, testcontainers.BindMount(
+			filepath.Join(p, "../protocol"),
+			"/protocol",
+		))
+		req.Mounts = append(req.Mounts, testcontainers.BindMount(
+			filepath.Join(p, "../verifier"),
+			"/verifier",
+		))
+
+		// The main binary is in common.
+		req.Mounts = append(req.Mounts, testcontainers.BindMount(
+			filepath.Join(p, "../common"),
+			AppPathInsideContainer,
+		))
 		req.Mounts = append(req.Mounts, GoCacheMounts()...)
 		framework.L.Info().
 			Str("Service", in.ContainerName).
