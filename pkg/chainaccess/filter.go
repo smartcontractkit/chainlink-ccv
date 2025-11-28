37421fd57394c3de2d8be9fd79c26a034158e169
Mateusz Sekara <mateusz.sekara@gmail.com>
2025-11-28 14:05:28 +0100

    CCIP-8341 Passing TXHash and enabling events filtering in SourceChainReader service (#379)

--- pkg/chainaccess/filter.go
+++ pkg/chainaccess/filter.go
@@ -2,29 +2,46 @@ package chainaccess
 
 import "github.com/smartcontractkit/chainlink-ccv/protocol"
 
-// ReceiptIssuerFilter filters protocol.MessageSentEvent to only include those
-// that have at least one of issuerAddresses present in their receipts.
-type ReceiptIssuerFilter struct {
-	issuerAddresses map[string]struct{}
+// VerifierIssuerFilter filters protocol.MessageSentEvent to only include those
+// that have at least one receipt issued by the specified verifier issuer address.
+type VerifierIssuerFilter struct {
+	issuerAddress protocol.UnknownAddress
 }
 
-func NewReceiptIssuerFilter(
-	issuerAddress ...protocol.UnknownAddress,
+func NewVerifierIssuerFilter(
+	issuerAddress protocol.UnknownAddress,
 ) MessageFilter {
-	issuerAddresses := make(map[string]struct{})
-	for _, addr := range issuerAddress {
-		issuerAddresses[addr.String()] = struct{}{}
-	}
-	return &ReceiptIssuerFilter{
-		issuerAddresses: issuerAddresses,
+	return &VerifierIssuerFilter{
+		issuerAddress: issuerAddress,
 	}
 }
 
-func (v *ReceiptIssuerFilter) Filter(msg protocol.MessageSentEvent) bool {
+func (v *VerifierIssuerFilter) Filter(msg protocol.MessageSentEvent) bool {
 	for _, receipt := range msg.Receipts {
-		if _, exists := v.issuerAddresses[receipt.Issuer.String()]; exists {
+		if v.issuerAddress.Equal(receipt.Issuer) {
 			return true
 		}
 	}
 	return false
+}
+
+// CompositeMessageFilter combines multiple MessageFilters using logical AND.
+// Works as allow-all if no filters are provided.
+type CompositeMessageFilter struct {
+	filters []MessageFilter
+}
+
+func NewCompositeMessageFilter(filters ...MessageFilter) MessageFilter {
+	return &CompositeMessageFilter{
+		filters: filters,
+	}
+}
+
+func (c *CompositeMessageFilter) Filter(msg protocol.MessageSentEvent) bool {
+	for _, filter := range c.filters {
+		if !filter.Filter(msg) {
+			return false
+		}
+	}
+	return true
 }
--- pkg/chainaccess/filter_test.go
+++ pkg/chainaccess/filter_test.go
@@ -12,25 +12,24 @@ func TestVerifierIssuerFilter_Filter(t *testing.T) {
 	targetIssuer := protocol.UnknownAddress("0x1234567890abcdef")
 	otherIssuer1 := protocol.UnknownAddress("0xabcdef1234567890")
 	otherIssuer2 := protocol.UnknownAddress("0xfedcba0987654321")
-	otherIssuer3 := protocol.UnknownAddress("0x1111111111111111")
 
 	tests := []struct {
 		name     string
-		issuers  []protocol.UnknownAddress
+		issuer   protocol.UnknownAddress
 		msg      protocol.MessageSentEvent
 		expected bool
 	}{
 		{
-			name:    "single issuer - no receipts - should return false",
-			issuers: []protocol.UnknownAddress{targetIssuer},
+			name:   "no receipts - should return false",
+			issuer: targetIssuer,
 			msg: protocol.MessageSentEvent{
 				Receipts: []protocol.ReceiptWithBlob{},
 			},
 			expected: false,
 		},
 		{
-			name:    "single issuer - single receipt with matching issuer - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer},
+			name:   "single receipt with matching issuer - should return true",
+			issuer: targetIssuer,
 			msg: protocol.MessageSentEvent{
 				Receipts: []protocol.ReceiptWithBlob{
 					{Issuer: targetIssuer},
@@ -39,8 +38,8 @@ func TestVerifierIssuerFilter_Filter(t *testing.T) {
 			expected: true,
 		},
 		{
-			name:    "single issuer - single receipt with non-matching issuer - should return false",
-			issuers: []protocol.UnknownAddress{targetIssuer},
+			name:   "single receipt with non-matching issuer - should return false",
+			issuer: targetIssuer,
 			msg: protocol.MessageSentEvent{
 				Receipts: []protocol.ReceiptWithBlob{
 					{Issuer: otherIssuer1},
@@ -49,8 +48,8 @@ func TestVerifierIssuerFilter_Filter(t *testing.T) {
 			expected: false,
 		},
 		{
-			name:    "single issuer - multiple receipts with matching issuer as first - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer},
+			name:   "multiple receipts with matching issuer as first - should return true",
+			issuer: targetIssuer,
 			msg: protocol.MessageSentEvent{
 				Receipts: []protocol.ReceiptWithBlob{
 					{Issuer: targetIssuer},
@@ -61,8 +60,8 @@ func TestVerifierIssuerFilter_Filter(t *testing.T) {
 			expected: true,
 		},
 		{
-			name:    "single issuer - multiple receipts with matching issuer in middle - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer},
+			name:   "multiple receipts with matching issuer in middle - should return true",
+			issuer: targetIssuer,
 			msg: protocol.MessageSentEvent{
 				Receipts: []protocol.ReceiptWithBlob{
 					{Issuer: otherIssuer1},
@@ -73,8 +72,8 @@ func TestVerifierIssuerFilter_Filter(t *testing.T) {
 			expected: true,
 		},
 		{
-			name:    "single issuer - multiple receipts with matching issuer as last - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer},
+			name:   "multiple receipts with matching issuer as last - should return true",
+			issuer: targetIssuer,
 			msg: protocol.MessageSentEvent{
 				Receipts: []protocol.ReceiptWithBlob{
 					{Issuer: otherIssuer1},
@@ -85,8 +84,8 @@ func TestVerifierIssuerFilter_Filter(t *testing.T) {
 			expected: true,
 		},
 		{
-			name:    "single issuer - multiple receipts with no matching issuer - should return false",
-			issuers: []protocol.UnknownAddress{targetIssuer},
+			name:   "multiple receipts with no matching issuer - should return false",
+			issuer: targetIssuer,
 			msg: protocol.MessageSentEvent{
 				Receipts: []protocol.ReceiptWithBlob{
 					{Issuer: otherIssuer1},
@@ -96,8 +95,8 @@ func TestVerifierIssuerFilter_Filter(t *testing.T) {
 			expected: false,
 		},
 		{
-			name:    "single issuer - multiple receipts with duplicate matching issuers - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer},
+			name:   "multiple receipts with duplicate matching issuers - should return true",
+			issuer: targetIssuer,
 			msg: protocol.MessageSentEvent{
 				Receipts: []protocol.ReceiptWithBlob{
 					{Issuer: targetIssuer},
@@ -106,143 +105,135 @@ func TestVerifierIssuerFilter_Filter(t *testing.T) {
 				},
 			},
 			expected: true,
-		},
-
-		{
-			name:    "multiple issuers - no receipts - should return false",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{},
-			},
-			expected: false,
-		},
-		{
-			name:    "multiple issuers - all issuers present in receipts - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: targetIssuer},
-					{Issuer: otherIssuer1},
-				},
-			},
-			expected: true,
-		},
-		{
-			name:    "multiple issuers - all issuers present with extras - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: targetIssuer},
-					{Issuer: otherIssuer1},
-					{Issuer: otherIssuer2},
-				},
-			},
-			expected: true,
-		},
-		{
-			name:    "multiple issuers - only first issuer present - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: targetIssuer},
-					{Issuer: otherIssuer2},
-				},
-			},
-			expected: true,
-		},
-		{
-			name:    "multiple issuers - only second issuer present - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: otherIssuer1},
-					{Issuer: otherIssuer2},
-				},
-			},
-			expected: true,
-		},
-		{
-			name:    "multiple issuers - none of the required issuers present - should return false",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: otherIssuer2},
-					{Issuer: otherIssuer3},
-				},
-			},
-			expected: false,
-		},
-		{
-			name:    "multiple issuers - all issuers present in different order - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: otherIssuer1},
-					{Issuer: targetIssuer},
-				},
-			},
-			expected: true,
-		},
-		{
-			name:    "three issuers - all present - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1, otherIssuer2},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: targetIssuer},
-					{Issuer: otherIssuer1},
-					{Issuer: otherIssuer2},
-				},
-			},
-			expected: true,
-		},
-		{
-			name:    "three issuers - two present (one missing) - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1, otherIssuer2},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: targetIssuer},
-					{Issuer: otherIssuer1},
-					{Issuer: otherIssuer3},
-				},
-			},
-			expected: true,
-		},
-		{
-			name:    "three issuers - all present with duplicates - should return true",
-			issuers: []protocol.UnknownAddress{targetIssuer, otherIssuer1, otherIssuer2},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: targetIssuer},
-					{Issuer: otherIssuer1},
-					{Issuer: targetIssuer}, // duplicate
-					{Issuer: otherIssuer2},
-				},
-			},
-			expected: true,
-		},
-		{
-			name:    "no issuers required - no receipts - should return false",
-			issuers: []protocol.UnknownAddress{},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{},
-			},
-			expected: false,
-		},
-		{
-			name:    "no issuers required - with receipts - should return false",
-			issuers: []protocol.UnknownAddress{},
-			msg: protocol.MessageSentEvent{
-				Receipts: []protocol.ReceiptWithBlob{
-					{Issuer: targetIssuer},
-				},
-			},
-			expected: false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			filter := NewReceiptIssuerFilter(tt.issuers...)
+			filter := NewVerifierIssuerFilter(tt.issuer)
+			result := filter.Filter(tt.msg)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func TestCompositeMessageFilter_Filter(t *testing.T) {
+	issuer1 := protocol.UnknownAddress("0x1111111111111111")
+	issuer2 := protocol.UnknownAddress("0x2222222222222222")
+	issuer3 := protocol.UnknownAddress("0x3333333333333333")
+
+	tests := []struct {
+		name     string
+		filters  []MessageFilter
+		msg      protocol.MessageSentEvent
+		expected bool
+	}{
+		{
+			name:    "no filters - should return true",
+			filters: []MessageFilter{},
+			msg: protocol.MessageSentEvent{
+				Receipts: []protocol.ReceiptWithBlob{},
+			},
+			expected: true,
+		},
+		{
+			name: "single filter matches - should return true",
+			filters: []MessageFilter{
+				NewVerifierIssuerFilter(issuer1),
+			},
+			msg: protocol.MessageSentEvent{
+				Receipts: []protocol.ReceiptWithBlob{
+					{Issuer: issuer1},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "single filter does not match - should return false",
+			filters: []MessageFilter{
+				NewVerifierIssuerFilter(issuer1),
+			},
+			msg: protocol.MessageSentEvent{
+				Receipts: []protocol.ReceiptWithBlob{
+					{Issuer: issuer2},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "multiple filters all match - should return true",
+			filters: []MessageFilter{
+				NewVerifierIssuerFilter(issuer1),
+				NewVerifierIssuerFilter(issuer2),
+			},
+			msg: protocol.MessageSentEvent{
+				Receipts: []protocol.ReceiptWithBlob{
+					{Issuer: issuer1},
+					{Issuer: issuer2},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "multiple filters first does not match - should return false",
+			filters: []MessageFilter{
+				NewVerifierIssuerFilter(issuer1),
+				NewVerifierIssuerFilter(issuer2),
+			},
+			msg: protocol.MessageSentEvent{
+				Receipts: []protocol.ReceiptWithBlob{
+					{Issuer: issuer2},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "multiple filters second does not match - should return false",
+			filters: []MessageFilter{
+				NewVerifierIssuerFilter(issuer1),
+				NewVerifierIssuerFilter(issuer2),
+			},
+			msg: protocol.MessageSentEvent{
+				Receipts: []protocol.ReceiptWithBlob{
+					{Issuer: issuer1},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "multiple filters none match - should return false",
+			filters: []MessageFilter{
+				NewVerifierIssuerFilter(issuer1),
+				NewVerifierIssuerFilter(issuer2),
+			},
+			msg: protocol.MessageSentEvent{
+				Receipts: []protocol.ReceiptWithBlob{
+					{Issuer: issuer3},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "three filters all match - should return true",
+			filters: []MessageFilter{
+				NewVerifierIssuerFilter(issuer1),
+				NewVerifierIssuerFilter(issuer2),
+				NewVerifierIssuerFilter(issuer3),
+			},
+			msg: protocol.MessageSentEvent{
+				Receipts: []protocol.ReceiptWithBlob{
+					{Issuer: issuer1},
+					{Issuer: issuer2},
+					{Issuer: issuer3},
+				},
+			},
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			filter := NewCompositeMessageFilter(tt.filters...)
 			result := filter.Filter(tt.msg)
 			assert.Equal(t, tt.expected, result)
 		})
