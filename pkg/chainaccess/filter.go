ec3d885ae64bf7c19b56fdb1542161d46cb59e86
Mateusz Sekara <mateusz.sekara@gmail.com>
2025-11-28 15:53:43 +0100

    CCIP-8341 Wiring up filter to Committee Verifier (#382)

--- pkg/chainaccess/filter.go
+++ pkg/chainaccess/filter.go
@@ -2,46 +2,29 @@ package chainaccess
 
 import "github.com/smartcontractkit/chainlink-ccv/protocol"
 
-// VerifierIssuerFilter filters protocol.MessageSentEvent to only include those
-// that have at least one receipt issued by the specified verifier issuer address.
-type VerifierIssuerFilter struct {
-	issuerAddress protocol.UnknownAddress
+// ReceiptIssuerFilter filters protocol.MessageSentEvent to only include those
+// that have at least one of issuerAddresses present in their receipts.
+type ReceiptIssuerFilter struct {
+	issuerAddresses map[string]struct{}
 }
 
-func NewVerifierIssuerFilter(
-	issuerAddress protocol.UnknownAddress,
+func NewReceiptIssuerFilter(
+	issuerAddress ...protocol.UnknownAddress,
 ) MessageFilter {
-	return &VerifierIssuerFilter{
-		issuerAddress: issuerAddress,
+	issuerAddresses := make(map[string]struct{})
+	for _, addr := range issuerAddress {
+		issuerAddresses[addr.String()] = struct{}{}
+	}
+	return &ReceiptIssuerFilter{
+		issuerAddresses: issuerAddresses,
 	}
 }
 
-func (v *VerifierIssuerFilter) Filter(msg protocol.MessageSentEvent) bool {
+func (v *ReceiptIssuerFilter) Filter(msg protocol.MessageSentEvent) bool {
 	for _, receipt := range msg.Receipts {
-		if v.issuerAddress.Equal(receipt.Issuer) {
+		if _, exists := v.issuerAddresses[receipt.Issuer.String()]; exists {
 			return true
 		}
 	}
 	return false
-}
-
-// CompositeMessageFilter combines multiple MessageFilters using logical AND.
-// Works as allow-all if no filters are provided.
-type CompositeMessageFilter struct {
-	filters []MessageFilter
-}
-
-func NewCompositeMessageFilter(filters ...MessageFilter) MessageFilter {
-	return &CompositeMessageFilter{
-		filters: filters,
-	}
-}
-
-func (c *CompositeMessageFilter) Filter(msg protocol.MessageSentEvent) bool {
-	for _, filter := range c.filters {
-		if !filter.Filter(msg) {
-			return false
-		}
-	}
-	return true
 }
