6d29470bd11d84291ecc661d5e16eb02adc0197b
Makram Kamaleddine <makramkd@users.noreply.github.com>
2025-12-02 16:35:39 +0000

    less parsing boilerplate, move to separate pkg

--- build/devenv/cmd/ccv/ccv.go
+++ build/devenv/cmd/ccv/ccv.go
@@ -19,7 +19,6 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 
 	chainsel "github.com/smartcontractkit/chain-selectors"
@@ -29,6 +28,7 @@ import (
 	onrampoperations "github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/operations/onramp"
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_0/operations/rmn_remote"
 	"github.com/smartcontractkit/chainlink-ccv/devenv/cciptestinterfaces"
+	"github.com/smartcontractkit/chainlink-ccv/devenv/cmd/ccv/manualexec"
 	"github.com/smartcontractkit/chainlink-ccv/devenv/services"
 	"github.com/smartcontractkit/chainlink-ccv/protocol"
 	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
@@ -722,105 +722,6 @@ var fundAddressesCmd = &cobra.Command{
 	},
 }
 
-var manuallyExecuteCmd = &cobra.Command{
-	Use:   "manually-execute",
-	Short: "Manually execute a message",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		chainSelector, err := cmd.Flags().GetUint64("chain-selector")
-		if err != nil {
-			return fmt.Errorf("failed to parse chain-selector: %w", err)
-		}
-		messageID, err := cmd.Flags().GetString("message-id")
-		if err != nil {
-			return fmt.Errorf("failed to parse message-id: %w", err)
-		}
-		env, err := cmd.Flags().GetString("env")
-		if err != nil {
-			return fmt.Errorf("failed to parse env: %w", err)
-		}
-		// TODO: once the indexer persists message ID results indefinitely we can change indexerResultJSON to indexerURL
-		// and fetch the result from the indexer directly.
-		indexerResultJSON, err := cmd.Flags().GetString("indexer-result-json")
-		if err != nil {
-			return fmt.Errorf("failed to parse indexer-result-json: %w", err)
-		}
-		gasLimit, err := cmd.Flags().GetUint64("gas-limit")
-		if err != nil {
-			return fmt.Errorf("failed to parse gas-limit: %w", err)
-		}
-		envFile := fmt.Sprintf("env-%s.toml", env)
-		in, err := ccv.LoadOutput[ccv.Cfg](envFile)
-		if err != nil {
-			return fmt.Errorf("failed to load environment output: %w", err)
-		}
-
-		var messageIDBytes32 protocol.Bytes32
-		messageIDBytes := hexutil.MustDecode(messageID)
-		copy(messageIDBytes32[:], messageIDBytes)
-
-		var resp protocol.MessageIDV1Response
-		err = json.Unmarshal([]byte(indexerResultJSON), &resp)
-		if err != nil {
-			return fmt.Errorf("failed to unmarshal indexer result JSON: %w", err)
-		}
-
-		msg := resp.Results[0].VerifierResult.Message
-		ccvs := make([]protocol.UnknownAddress, 0, len(resp.Results))
-		verifierResults := make([][]byte, 0, len(resp.Results))
-		for _, result := range resp.Results {
-			ccvs = append(ccvs, result.VerifierResult.VerifierDestAddress)
-			verifierResults = append(verifierResults, result.VerifierResult.CCVData)
-		}
-
-		chainID, err := chainsel.ChainIdFromSelector(chainSelector)
-		if err != nil {
-			return fmt.Errorf("failed to get chain details: %w", err)
-		}
-		chainIDStr := strconv.FormatUint(chainID, 10)
-
-		// get the blockchain input for the chain selector
-		var input *blockchain.Input
-		for _, bc := range in.Blockchains {
-			if bc.ChainID == chainIDStr {
-				input = bc
-				break
-			}
-		}
-		if input == nil {
-			return fmt.Errorf("blockchain with chain ID %s not found, please update the env file or use a different chain-selector", chainIDStr)
-		}
-
-		// TODO: this kind of thing should be chain-agnostic.
-		chainIDs, wsURLs := make([]string, 0), make([]string, 0)
-		for _, bc := range in.Blockchains {
-			chainIDs = append(chainIDs, bc.ChainID)
-			wsURLs = append(wsURLs, bc.Out.Nodes[0].ExternalWSUrl)
-		}
-		_, e, err := ccv.NewCLDFOperationsEnvironment(in.Blockchains, in.CLDF.DataStore)
-		if err != nil {
-			return fmt.Errorf("failed to create CLDF operations environment: %w", err)
-		}
-		l := log.
-			Output(zerolog.ConsoleWriter{Out: os.Stderr}).
-			Level(zerolog.DebugLevel).
-			With().
-			Fields(map[string]any{"component": "CCIP17EVM"}).
-			Logger()
-		impl, err := evm.NewCCIP17EVM(cmd.Context(), l, e, chainIDs, wsURLs)
-		if err != nil {
-			return fmt.Errorf("failed to create CCIP17EVM: %w", err)
-		}
-		// END TODO.
-
-		_, err = impl.ManuallyExecuteMessage(cmd.Context(), msg, gasLimit, ccvs, verifierResults)
-		if err != nil {
-			return fmt.Errorf("failed to manually execute message: %w", err)
-		}
-
-		return nil
-	},
-}
-
 var monitorContractsCmd = &cobra.Command{
 	Use:   "upload-on-chain-metrics <source> <dest>",
 	Short: "Reads on-chain EVM contract events and temporary exposes them as Prometheus metrics endpoint to be scraped",
@@ -1115,17 +1016,7 @@ func init() {
 	_ = generateConfigsCmd.MarkFlagRequired("monitoring-otel-exporter-http-endpoint")
 
 	// manual execution
-	rootCmd.AddCommand(manuallyExecuteCmd)
-	manuallyExecuteCmd.Flags().String("env", "out", "Select environment file to use (e.g., 'staging' for env-staging.toml, defaults to env-out.toml)")
-	manuallyExecuteCmd.Flags().Uint64("chain-selector", 0, "Chain selector to manually execute message for")
-	manuallyExecuteCmd.Flags().String("message-id", "", "Message ID to manually execute")
-	manuallyExecuteCmd.Flags().String("indexer-result-json", "", "Indexer result JSON to manually execute message from")
-	manuallyExecuteCmd.Flags().Uint64("gas-limit", 0, "Gas limit to use for the manually executed message")
-
-	_ = manuallyExecuteCmd.MarkFlagRequired("chain-selector")
-	_ = manuallyExecuteCmd.MarkFlagRequired("message-id")
-	_ = manuallyExecuteCmd.MarkFlagRequired("indexer-result-json")
-	_ = manuallyExecuteCmd.MarkFlagRequired("gas-limit")
+	rootCmd.AddCommand(manualexec.Command())
 }
 
 func checkDockerIsRunning() {
