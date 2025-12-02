05cfa3307fa901d77ea939dd24b5303db68610f1
Kodey Kilday-Thomas <kodey.kildaythomas@smartcontract.com>
2025-12-02 10:58:08 +0000

    fix: indexer deadlock

--- indexer/pkg/worker/worker_pool.go
+++ indexer/pkg/worker/worker_pool.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"github.com/panjf2000/ants/v2"
@@ -20,6 +21,8 @@ type Pool struct {
 	scheduler        *Scheduler
 	registry         *registry.VerifierRegistry
 	storage          common.IndexerStorage
+	wg               sync.WaitGroup
+	cancelFunc       context.CancelFunc
 }
 
 // NewWorkerPool creates a new WorkerPool with the given configuration.
@@ -42,33 +45,31 @@ func NewWorkerPool(logger logger.Logger, config config.PoolConfig, discoveryChan
 
 // Start begins processing messages from the discovery channel.
 func (p *Pool) Start(ctx context.Context) {
-	go p.run(ctx)
+	p.wg.Add(3)
+	childCtx, cancelFunc := context.WithCancel(ctx)
+	p.cancelFunc = cancelFunc
+
+	go p.run(childCtx)
+	go p.enqueueMessages(childCtx)
+	go p.handleDLQ(childCtx)
+}
+
+func (p *Pool) Stop() {
+	p.logger.Info("Stopping WorkerPool")
+	p.cancelFunc()
+	p.wg.Wait()
+	p.logger.Info("Stopepd WorkerPool")
 }
 
 func (p *Pool) run(ctx context.Context) {
 	defer p.pool.Release()
+	defer p.wg.Done()
 
 	for {
 		select {
 		case <-ctx.Done():
+			p.logger.Info("Exiting WorkerPool, due to context cancellation")
 			return
-		case message, ok := <-p.discoveryChannel:
-			if !ok {
-				continue
-			}
-			p.logger.Infow("Enqueueing new Message", "messageID", message.VerifierResult.MessageID.String())
-			task, err := NewTask(p.logger, message.VerifierResult, p.registry, p.storage, p.scheduler.VerificationVisibilityWindow())
-			// This shouldn't happen, it can only be caused by an invalid hex conversion.
-			// We're unable to retry the message or send it to the DLQ.
-			if err != nil {
-				p.logger.Error("Unable to create Task. this shouldn't happen.", err)
-				continue
-			}
-
-			if err = p.scheduler.Enqueue(ctx, task); err != nil {
-				p.logger.Errorf("Unable to enqueue: %v", err)
-				continue
-			}
 		case task, ok := <-p.scheduler.Ready():
 			if !ok {
 				continue
@@ -104,12 +105,53 @@ func (p *Pool) run(ctx context.Context) {
 					p.logger.Errorf("Unable to enqueue: %v", err)
 				}
 			}
+		}
+	}
+}
+
+func (p *Pool) enqueueMessages(ctx context.Context) {
+	defer p.wg.Done()
+
+	for {
+		select {
+		case <-ctx.Done():
+			p.logger.Info("Exiting EnqueueMessages")
+			return
+		case message, ok := <-p.discoveryChannel:
+			if !ok {
+				continue
+			}
+			p.logger.Infow("Enqueueing new Message", "messageID", message.VerifierResult.MessageID.String())
+			task, err := NewTask(p.logger, message.VerifierResult, p.registry, p.storage, p.scheduler.VerificationVisibilityWindow())
+			// This shouldn't happen, it can only be caused by an invalid hex conversion.
+			// We're unable to retry the message or send it to the DLQ.
+			if err != nil {
+				p.logger.Error("Unable to create Task. this shouldn't happen.", err)
+				continue
+			}
+
+			if err = p.scheduler.Enqueue(ctx, task); err != nil {
+				p.logger.Errorf("Unable to enqueue: %v", err)
+				continue
+			}
+		}
+	}
+}
+
+func (p *Pool) handleDLQ(ctx context.Context) {
+	defer p.wg.Done()
+
+	for {
+		select {
+		case <-ctx.Done():
+			p.logger.Info("Exiting DLQ Queue")
+			return
 		case task, ok := <-p.scheduler.DLQ():
 			if !ok {
 				continue
 			}
 			p.logger.Warnf("Message %s entered DLQ. Partial verifications may have been recieved", task.messageID.String())
-			// TODO: DLQ Logic here...
+			// TODO: DLQ Logic here..
 		}
 	}
 }
