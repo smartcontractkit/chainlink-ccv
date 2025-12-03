961987b575567f52ee4a3ebc31b80dccd886abbf
Abdelrahman Soliman (Boda) <2677789+asoliman92@users.noreply.github.com>
2025-12-03 14:25:42 +0000

    Add parent hash check to detect mid fetch violations (#414)
    * Add parent hash check to detect mid fetch violations
    
    * Fix tests by adding parent hash

--- verifier/services/finality_violation_checker.go
+++ verifier/services/finality_violation_checker.go
@@ -136,20 +136,8 @@ func (f *FinalityViolationCheckerService) UpdateFinalized(ctx context.Context, f
 			return fmt.Errorf("missing header for block %d in fetched range", blockNum)
 		}
 
-		// Check if we already have this block stored
-		if storedHeader, ok := f.finalizedBlocks[blockNum]; ok {
-			// Verify hash matches
-			if storedHeader.Hash != newHeader.Hash {
-				f.lggr.Errorw("FINALITY VIOLATION DETECTED - block hash changed",
-					"blockNumber", blockNum,
-				)
-				f.violationDetected = true
-				return fmt.Errorf("finality violation: block %d hash changed from %s to %s",
-					blockNum, storedHeader.Hash, newHeader.Hash)
-			}
-		} else {
-			// Store new finalized block
-			f.finalizedBlocks[blockNum] = newHeader
+		if err := f.validateAndStore(blockNum, newHeader); err != nil {
+			return err
 		}
 	}
 
@@ -175,6 +163,44 @@ func (f *FinalityViolationCheckerService) trimStoredBlocks() {
 			delete(f.finalizedBlocks, blockNum)
 		}
 	}
+}
+
+// validateAndStore checks block hash consistency and parent hash continuity.
+// Returns error and sets violationDetected if a finality violation is found.
+func (f *FinalityViolationCheckerService) validateAndStore(blockNum uint64, newHeader protocol.BlockHeader) error {
+	// Check if we already have this block stored
+	if storedHeader, ok := f.finalizedBlocks[blockNum]; ok {
+		if storedHeader.Hash != newHeader.Hash {
+			f.lggr.Errorw("FINALITY VIOLATION DETECTED - block hash changed",
+				"blockNumber", blockNum,
+				"storedHash", storedHeader.Hash,
+				"newHash", newHeader.Hash,
+			)
+			f.violationDetected = true
+			return fmt.Errorf("finality violation: block %d hash changed from %s to %s",
+				blockNum, storedHeader.Hash, newHeader.Hash)
+		}
+		return nil
+	}
+
+	// New block: verify parent hash matches the previous stored block
+	if blockNum > 0 {
+		if prevHeader, hasPrev := f.finalizedBlocks[blockNum-1]; hasPrev {
+			if newHeader.ParentHash != prevHeader.Hash {
+				f.lggr.Errorw("FINALITY VIOLATION DETECTED - parent hash mismatch",
+					"blockNumber", blockNum,
+					"expectedParent", prevHeader.Hash,
+					"actualParent", newHeader.ParentHash,
+				)
+				f.violationDetected = true
+				return fmt.Errorf("finality violation: block %d parent hash %s doesn't match block %d hash %s",
+					blockNum, newHeader.ParentHash, blockNum-1, prevHeader.Hash)
+			}
+		}
+	}
+
+	f.finalizedBlocks[blockNum] = newHeader
+	return nil
 }
 
 // IsFinalityViolated returns true if a finality violation has been detected.
