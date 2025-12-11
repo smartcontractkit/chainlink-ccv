4b81f461d1ad060a84cfe40603587c01d80b649f
Andres Julia <andres.julia@smartcontract.com>
2025-12-11 11:57:14 -0300

    Fix ccv gencfg

--- build/devenv/gencfg/gencfg.go
+++ build/devenv/gencfg/gencfg.go
@@ -188,7 +188,7 @@ func GenerateConfigs(cldDomain string, verifierPubKeys []string, numExecutors in
 	ccv.Plog.Info().Str("file-path", filePath).Msg("Wrote aggregator config to file")
 
 	if createPR {
-		prURL, err := createConfigPR(gh, ctx, tempDir, cldDomain, aggregatorConfig)
+		prURL, err := createConfigPR(gh, ctx, cldDomain, aggregatorConfig)
 		if err != nil {
 			return "", fmt.Errorf("failed to create config PR: %w", err)
 		}
@@ -198,7 +198,7 @@ func GenerateConfigs(cldDomain string, verifierPubKeys []string, numExecutors in
 	return tempDir, nil
 }
 
-func createConfigPR(gh *github.Client, ctx context.Context, tempDir, cldDomain string, aggregatorConfig []byte) (string, error) {
+func createConfigPR(gh *github.Client, ctx context.Context, cldDomain string, aggregatorConfig []byte) (string, error) {
 	// Create a new branch, add the aggregator config file and open a PR
 	owner := "smartcontractkit"
 	repo := "infra-k8s"
@@ -239,8 +239,8 @@ func createConfigPR(gh *github.Client, ctx context.Context, tempDir, cldDomain s
 
 	// Marshal aggregator config into YAML under configMap.aggregator.\.toml
 	aggYaml := map[string]any{
-		"main": map[string]any{
-			"stage": map[string]any{
+		"stage": map[string]any{
+			"main": map[string]any{
 				"configMap": map[string]string{
 					"aggregator.toml": string(aggregatorConfig),
 				},
@@ -252,10 +252,14 @@ func createConfigPR(gh *github.Client, ctx context.Context, tempDir, cldDomain s
 		return "", fmt.Errorf("failed to marshal aggregator config to YAML: %w", err)
 	}
 
+	aggFile, _, _, _ := gh.Repositories.GetContents(ctx, "smartcontractkit", "infra-k8s", fmt.Sprintf("projects/chainlink-ccv/files/aggregator/aggregator-config.yaml"), &github.RepositoryContentGetOptions{})
+	aggSHA := aggFile.GetSHA()
+
 	opts := &github.RepositoryContentFileOptions{
 		Message: github.Ptr(commitMsg),
 		Content: aggFileContent,
 		Branch:  github.Ptr(branchName),
+		SHA:     github.Ptr(aggSHA),
 	}
 	_, _, err = gh.Repositories.CreateFile(ctx, owner, repo, aggPath, opts)
 	if err != nil {
