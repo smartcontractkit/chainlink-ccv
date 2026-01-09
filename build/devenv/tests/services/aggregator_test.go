e6681b29d2b735fcb4eec8694e7d3ae401ee7b6f
Simon B.Robert <simon.robert@smartcontract.com>
2026-01-09 08:07:38 -0500

    Change test name for CI run

--- build/devenv/tests/services/aggregator_test.go
+++ build/devenv/tests/services/aggregator_test.go
@@ -578,7 +578,7 @@ func setupAggregatorTestFixture(t *testing.T) *aggregatorTestFixture {
 	}
 }
 
-func TestAggregatorSecurityFeatures(t *testing.T) {
+func TestServiceAggregatorSecurityFeatures(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
