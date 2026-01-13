7c605e82d4e78c5eff8b3cfe12243e13d27915b8
Austin <107539019+0xAustinWang@users.noreply.github.com>
2026-01-13 12:51:44 -0500

    Aw/multiuser load (#545)
    * working multi chain test
    
    * add metrics for source/dest chain
    
    * multi chain test in staging
    
    * add multiple users
    
    * add import, sort
    
    * fix lint
    
    * lint on unrelated files
    
    * fix merge
    
    * add creation of user based load test to clean test as well
    
    * add sender to log
    
    * add deployer key to user, make things more adaptable
    
    * lint

--- build/devenv/tests/e2e/gun.go
+++ build/devenv/tests/e2e/gun.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/Masterminds/semver/v3"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/operations/committee_verifier"
@@ -32,6 +33,11 @@ type SrcDest struct {
 	Dest uint64
 }
 
+type NonceKey struct {
+	Selector uint64
+	Address  string
+}
+
 // SentMessage represents a message that was sent and needs verification.
 type SentMessage struct {
 	SeqNo     uint64
@@ -41,19 +47,21 @@ type SentMessage struct {
 }
 
 type EVMTXGun struct {
-	cfg           *ccv.Cfg
-	testConfig    *load.TestProfileConfig
-	e             *deployment.Environment
-	selectors     []uint64
-	impl          map[uint64]cciptestinterfaces.CCIP17
-	sentMsgSet    map[SentMessage]struct{}
-	srcSelectors  []uint64
-	destSelectors []uint64
-	seqNosMu      sync.Mutex
-	sentMsgCh     chan SentMessage // Channel for real-time message notifications
-	closeOnce     sync.Once        // Ensure channel is closed only once
-	nonceMu       sync.Mutex
-	nonce         map[uint64]*atomic.Uint64
+	cfg             *ccv.Cfg
+	testConfig      *load.TestProfileConfig
+	e               *deployment.Environment
+	selectors       []uint64
+	impl            map[uint64]cciptestinterfaces.CCIP17
+	sentMsgSet      map[SentMessage]struct{}
+	srcSelectors    []uint64
+	destSelectors   []uint64
+	seqNosMu        sync.Mutex
+	sentMsgCh       chan SentMessage // Channel for real-time message notifications
+	closeOnce       sync.Once        // Ensure channel is closed only once
+	nonceMu         sync.Mutex
+	nonce           map[NonceKey]*atomic.Uint64
+	messageProfiles []load.MessageProfileConfig
+	userSelector    map[uint64]func() *bind.TransactOpts
 }
 
 // CloseSentChannel closes the sent messages channel to signal no more messages will be sent.
@@ -64,6 +72,10 @@ func (m *EVMTXGun) CloseSentChannel() {
 }
 
 func NewEVMTransactionGun(cfg *ccv.Cfg, e *deployment.Environment, selectors []uint64, impls map[uint64]cciptestinterfaces.CCIP17, srcSelectors, destSelectors []uint64) *EVMTXGun {
+	userSelector := make(map[uint64]func() *bind.TransactOpts)
+	for _, chain := range srcSelectors {
+		userSelector[chain] = impls[chain].GetRoundRobinUser()
+	}
 	return &EVMTXGun{
 		cfg:           cfg,
 		e:             e,
@@ -71,122 +83,102 @@ func NewEVMTransactionGun(cfg *ccv.Cfg, e *deployment.Environment, selectors []u
 		impl:          impls,
 		sentMsgSet:    make(map[SentMessage]struct{}),
 		sentMsgCh:     make(chan SentMessage, sentMessageChannelBufferSize),
-		nonce:         make(map[uint64]*atomic.Uint64),
+		nonce:         make(map[NonceKey]*atomic.Uint64),
 		srcSelectors:  srcSelectors,
 		destSelectors: destSelectors,
+		userSelector:  userSelector,
 	}
 }
 
-func NewEVMTransactionGunFromTestConfig(cfg *ccv.Cfg, testConfig *load.TestProfileConfig, e *deployment.Environment, impls map[uint64]cciptestinterfaces.CCIP17) *EVMTXGun {
+func NewEVMTransactionGunFromTestConfig(cfg *ccv.Cfg, testConfig *load.TOMLLoadTestRoot, e *deployment.Environment, impls map[uint64]cciptestinterfaces.CCIP17) *EVMTXGun {
+	testProfile := testConfig.TestProfiles[0]
 	selectors := make([]uint64, 0)
-	srcSelectors := make([]uint64, 0, len(testConfig.ChainsAsSource))
-	destSelectors := make([]uint64, 0, len(testConfig.ChainsAsDest))
-	for _, chain := range testConfig.ChainsAsSource {
-		chainSelector, _ := strconv.ParseUint(chain, 10, 64)
+	srcSelectors := make([]uint64, 0, len(testProfile.ChainsAsSource))
+	destSelectors := make([]uint64, 0, len(testProfile.ChainsAsDest))
+	for _, chain := range testProfile.ChainsAsSource {
+		chainSelector, _ := strconv.ParseUint(chain.Selector, 10, 64)
 		selectors = append(selectors, chainSelector)
 		srcSelectors = append(srcSelectors, chainSelector)
 	}
-	for _, chain := range testConfig.ChainsAsDest {
-		chainSelector, _ := strconv.ParseUint(chain, 10, 64)
+	for _, chain := range testProfile.ChainsAsDest {
+		chainSelector, _ := strconv.ParseUint(chain.Selector, 10, 64)
 		selectors = append(selectors, chainSelector)
 		destSelectors = append(destSelectors, chainSelector)
 	}
+
+	userSelector := make(map[uint64]func() *bind.TransactOpts)
+	for _, chain := range srcSelectors {
+		userSelector[chain] = impls[chain].GetRoundRobinUser()
+	}
+
 	return &EVMTXGun{
-		cfg:           cfg,
-		testConfig:    testConfig,
-		e:             e,
-		selectors:     selectors,
-		impl:          impls,
-		sentMsgSet:    make(map[SentMessage]struct{}),
-		sentMsgCh:     make(chan SentMessage, sentMessageChannelBufferSize),
-		nonce:         make(map[uint64]*atomic.Uint64),
-		srcSelectors:  srcSelectors,
-		destSelectors: destSelectors,
+		cfg:             cfg,
+		testConfig:      &testProfile,
+		e:               e,
+		selectors:       selectors,
+		impl:            impls,
+		sentMsgSet:      make(map[SentMessage]struct{}),
+		sentMsgCh:       make(chan SentMessage, sentMessageChannelBufferSize),
+		nonce:           make(map[NonceKey]*atomic.Uint64),
+		srcSelectors:    srcSelectors,
+		destSelectors:   destSelectors,
+		messageProfiles: testConfig.MessageProfiles,
+		userSelector:    userSelector,
 	}
 }
 
-func (m *EVMTXGun) initNonce(chainSelector uint64) error {
+func (m *EVMTXGun) initNonce(key NonceKey) error {
 	m.nonceMu.Lock()
 	defer m.nonceMu.Unlock()
 
-	if m.nonce[chainSelector] != nil {
+	if m.nonce[key] != nil {
 		return nil
 	}
 
-	n, err := m.impl[chainSelector].GetUserNonce(context.Background())
+	n, err := m.impl[key.Selector].GetUserNonce(context.Background())
 	if err != nil {
-		return fmt.Errorf("failed to get pending nonce for selector %d: %w", chainSelector, err)
+		return fmt.Errorf("failed to get pending nonce for selector %d: %w", key.Selector, err)
 	}
 
-	m.nonce[chainSelector] = &atomic.Uint64{}
-	m.nonce[chainSelector].Store(n)
+	m.nonce[key] = &atomic.Uint64{}
+	m.nonce[key].Store(n)
 	return nil
 }
 
 // Call implements example gun call, assertions on response bodies should be done here.
 func (m *EVMTXGun) Call(_ *wasp.Generator) *wasp.Response {
-	srcSelector := m.SelectSourceSelector()
-	destSelector := m.SelectDestinationSelector(srcSelector)
+	ctx := context.Background()
+	sentTime := time.Now()
+	srcSelector, err := m.SelectSourceSelector()
+	if err != nil {
+		return &wasp.Response{Error: fmt.Errorf("failed to select source selector: %w", err).Error(), Failed: true}
+	}
+	destSelector, err := m.SelectDestSelector(srcSelector)
+	if err != nil {
+		return &wasp.Response{Error: fmt.Errorf("failed to select dest selector: %w", err).Error(), Failed: true}
+	}
 
-	if err := m.initNonce(srcSelector); err != nil {
+	fields, opts, err := m.selectMessageProfile(srcSelector, destSelector)
+	if err != nil {
+		return &wasp.Response{Error: fmt.Errorf("failed to select message profile: %w", err).Error(), Failed: true}
+	}
+
+	sender := m.userSelector[srcSelector]()
+	nonceKey := NonceKey{Selector: srcSelector, Address: sender.From.String()}
+
+	if err := m.initNonce(nonceKey); err != nil {
 		return &wasp.Response{Error: err.Error(), Failed: true}
 	}
 
 	b := ccv.NewDefaultCLDFBundle(m.e)
 	m.e.OperationsBundle = b
-	ctx := context.Background()
 
 	c, ok := m.impl[srcSelector]
 	if !ok {
 		return &wasp.Response{Error: "impl is not CCIP17EVM", Failed: true}
 	}
 
-	sentTime := time.Now()
-
-	mockReceiverRef, err := m.e.DataStore.Addresses().Get(
-		datastore.NewAddressRefKey(
-			destSelector,
-			datastore.ContractType(mock_receiver.ContractType),
-			semver.MustParse(mock_receiver.Deploy.Version()),
-			evm.DefaultReceiverQualifier))
-	if err != nil {
-		return &wasp.Response{Error: fmt.Errorf("could not find mock receiver address in datastore: %w", err).Error(), Failed: true}
-	}
-	committeeVerifierProxyRef, err := m.e.DataStore.Addresses().Get(
-		datastore.NewAddressRefKey(
-			srcSelector,
-			datastore.ContractType(committee_verifier.ResolverType),
-			semver.MustParse(committee_verifier.Deploy.Version()),
-			evm.DefaultCommitteeVerifierQualifier))
-	if err != nil {
-		return &wasp.Response{Error: fmt.Errorf("could not find committee verifier proxy address in datastore: %w", err).Error(), Failed: true}
-	}
-
-	wethContract, err := m.e.DataStore.Addresses().Get(
-		datastore.NewAddressRefKey(
-			srcSelector,
-			datastore.ContractType(weth.ContractType),
-			semver.MustParse(weth.Deploy.Version()),
-			""))
-	if err != nil {
-		return &wasp.Response{Error: fmt.Errorf("could not find WETH address in datastore: %w", err).Error(), Failed: true}
-	}
-
-	sentEvent, err := c.SendMessageWithNonce(ctx, destSelector, cciptestinterfaces.MessageFields{
-		Receiver: protocol.UnknownAddress(common.HexToAddress(mockReceiverRef.Address).Bytes()),
-		Data:     []byte{},
-		FeeToken: protocol.UnknownAddress(common.HexToAddress(wethContract.Address).Bytes()),
-	}, cciptestinterfaces.MessageOptions{
-		Version:        3,
-		FinalityConfig: uint16(1),
-		CCVs: []protocol.CCV{
-			{
-				CCVAddress: common.HexToAddress(committeeVerifierProxyRef.Address).Bytes(),
-				Args:       []byte{},
-				ArgsLen:    0,
-			},
-		},
-	}, m.nonce[srcSelector], true)
+	sentEvent, err := c.SendMessageWithNonce(ctx, destSelector, fields, opts, sender, m.nonce[nonceKey], true)
 	if err != nil {
 		return &wasp.Response{Error: fmt.Errorf("failed to send message: %w", err).Error(), Failed: true}
 	}
@@ -202,10 +194,92 @@ func (m *EVMTXGun) Call(_ *wasp.Generator) *wasp.Response {
 	return &wasp.Response{Data: "ok"}
 }
 
-func (m *EVMTXGun) SelectSourceSelector() uint64 {
-	return m.srcSelectors[0]
+// SelectSourceSelectorByRatio selects an element from m.srcSelectors according to the source ratio in the chain_profiles.
+func (m *EVMTXGun) SelectSourceSelector() (uint64, error) {
+	if m.testConfig == nil {
+		return m.srcSelectors[0], nil
+	}
+	return load.GetSelectorByRatio(
+		m.testConfig.ChainsAsSource,
+	)
 }
 
-func (m *EVMTXGun) SelectDestinationSelector(srcSelector uint64) uint64 {
-	return m.destSelectors[0]
+// SelectDestSelectorByRatio selects an element from m.destSelectors according to the dest ratio in the chain_profiles.
+func (m *EVMTXGun) SelectDestSelector(excludeSelector uint64) (uint64, error) {
+	if m.testConfig == nil {
+		return m.destSelectors[0], nil
+	}
+	choices := make([]load.ChainProfileConfig, 0, len(m.testConfig.ChainsAsDest))
+	for _, chain := range m.testConfig.ChainsAsDest {
+		if chain.Selector != strconv.FormatUint(excludeSelector, 10) {
+			choices = append(choices, chain)
+		}
+	}
+	return load.GetSelectorByRatio(choices)
+}
+
+func (m *EVMTXGun) selectMessageProfile(srcSelector, destSelector uint64) (cciptestinterfaces.MessageFields, cciptestinterfaces.MessageOptions, error) {
+	mockReceiverRef, err := m.e.DataStore.Addresses().Get(
+		datastore.NewAddressRefKey(
+			destSelector,
+			datastore.ContractType(mock_receiver.ContractType),
+			semver.MustParse(mock_receiver.Deploy.Version()),
+			evm.DefaultReceiverQualifier))
+	if err != nil {
+		return cciptestinterfaces.MessageFields{}, cciptestinterfaces.MessageOptions{}, fmt.Errorf("could not find mock receiver address in datastore: %w", err)
+	}
+
+	wethContract, err := m.e.DataStore.Addresses().Get(
+		datastore.NewAddressRefKey(
+			srcSelector,
+			datastore.ContractType(weth.ContractType),
+			semver.MustParse(weth.Deploy.Version()),
+			""))
+	if err != nil {
+		return cciptestinterfaces.MessageFields{}, cciptestinterfaces.MessageOptions{}, fmt.Errorf("could not find WETH address in datastore: %w", err)
+	}
+
+	committeeVerifierProxyRef, err := m.e.DataStore.Addresses().Get(
+		datastore.NewAddressRefKey(
+			srcSelector,
+			datastore.ContractType(committee_verifier.ResolverType),
+			semver.MustParse(committee_verifier.Deploy.Version()),
+			evm.DefaultCommitteeVerifierQualifier))
+	if err != nil {
+		return cciptestinterfaces.MessageFields{}, cciptestinterfaces.MessageOptions{}, fmt.Errorf("could not find committee verifier proxy address in datastore: %w", err)
+	}
+
+	if m.testConfig == nil || m.testConfig.Messages == nil {
+		return cciptestinterfaces.MessageFields{
+				Receiver: protocol.UnknownAddress(common.HexToAddress(mockReceiverRef.Address).Bytes()),
+				Data:     []byte{},
+				FeeToken: protocol.UnknownAddress(common.HexToAddress(wethContract.Address).Bytes()),
+			}, cciptestinterfaces.MessageOptions{
+				Version:        3,
+				FinalityConfig: uint16(1),
+				CCVs: []protocol.CCV{
+					{
+						CCVAddress: common.HexToAddress(committeeVerifierProxyRef.Address).Bytes(),
+						Args:       []byte{},
+						ArgsLen:    0,
+					},
+				},
+			},
+			nil
+	}
+	messageProfile, err := load.GetMessageByRatio(m.testConfig.Messages, m.messageProfiles)
+	if err != nil {
+		return cciptestinterfaces.MessageFields{}, cciptestinterfaces.MessageOptions{}, fmt.Errorf("failed to get message profile: %w", err)
+	}
+	fields := cciptestinterfaces.MessageFields{
+		Receiver: protocol.UnknownAddress(common.HexToAddress(mockReceiverRef.Address).Bytes()),
+		Data:     []byte{},
+		FeeToken: protocol.UnknownAddress(common.HexToAddress(wethContract.Address).Bytes()),
+	}
+	opts := cciptestinterfaces.MessageOptions{
+		Version:        3,
+		FinalityConfig: uint16(messageProfile.Finality),
+	}
+
+	return fields, opts, nil
 }
--- build/devenv/tests/e2e/load/types.go
+++ build/devenv/tests/e2e/load/types.go
@@ -1,6 +1,10 @@
 package load
 
 import (
+	"fmt"
+	"math/rand/v2"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/BurntSushi/toml"
@@ -13,11 +17,10 @@ type MessageProfileConfig struct {
 	Name     string `toml:"name"`     // e.g., "data only"
 }
 
-// ChainProfileConfig represents an entry in chain_profiles in the TOML config.
+// ChainProfileConfig represents an chain in the test profile config.
 type ChainProfileConfig struct {
-	RatioAsSource int    `toml:"ratio_as_source"`
-	RatioAsDest   int    `toml:"ratio_as_dest"`
-	Name          string `toml:"name"` // e.g., "heavy"
+	Ratio    int    `toml:"ratio"`
+	Selector string `toml:"selector"` // e.g., "3379446385462418246"
 }
 
 // MessageTestRatio ties a message ratio to a specific message profile by name.
@@ -34,18 +37,17 @@ type ChainTestConfig struct {
 
 // TestProfileConfig represents each [test_profiles] block in the TOML config file.
 type TestProfileConfig struct {
-	ChainsAsSource    []string           `toml:"chains_as_source"`
-	ChainsAsDest      []string           `toml:"chains_as_dest"`
-	Chains            []ChainTestConfig  `toml:"chains"`
-	Messages          []MessageTestRatio `toml:"messages"`
-	TestDuration      time.Duration      `toml:"test_duration"`
-	LoadDuration      time.Duration      `toml:"load_duration"`
-	MessagesPerSecond int64              `toml:"messages_per_second"`
+	ChainsAsSource []ChainProfileConfig `toml:"chains_as_source"`
+	ChainsAsDest   []ChainProfileConfig `toml:"chains_as_dest"`
+	Chains         []ChainTestConfig    `toml:"chains"`
+	Messages       []MessageTestRatio   `toml:"messages"`
+	TestDuration   time.Duration        `toml:"test_duration"`
+	LoadDuration   time.Duration        `toml:"load_duration"`
+	MessageRate    string               `toml:"message_rate"`
 }
 
 // TOMLLoadTestRoot matches the structure of the TOML file for decoding.
 type TOMLLoadTestRoot struct {
-	ChainProfiles   []ChainProfileConfig   `toml:"chain_profiles"`
 	MessageProfiles []MessageProfileConfig `toml:"message_profiles"`
 	TestProfiles    []TestProfileConfig    `toml:"test_profiles"`
 }
@@ -57,4 +59,73 @@ func LoadTestConfigFromTomlFile(tomlPath string) (*TOMLLoadTestRoot, error) {
 		return nil, err
 	}
 	return &cfg, nil
+}
+
+// GetSelectorByRatio selects a chain selector from the given slice based on ratio (weighted random selection)
+// and returns it as uint64.
+func GetSelectorByRatio(profiles []ChainProfileConfig) (uint64, error) {
+	if len(profiles) == 0 {
+		return 0, fmt.Errorf("no profiles provided")
+	}
+
+	// Calculate totalWeight weight
+	totalWeight := 0
+	for _, p := range profiles {
+		totalWeight += p.Ratio
+	}
+	// Weighted random selection
+	r := rand.IntN(totalWeight)
+	accum := 0
+	for _, p := range profiles {
+		accum += p.Ratio
+		if r < accum {
+			selector, err := strconv.ParseUint(p.Selector, 10, 64)
+			if err != nil {
+				return 0, fmt.Errorf("failed to parse selector %s: %w", p.Selector, err)
+			}
+			return selector, nil
+		}
+	}
+	return 0, fmt.Errorf("no selector found after weighted random selection")
+}
+
+func GetMessageByRatio(profiles []MessageTestRatio, messageProfiles []MessageProfileConfig) (MessageProfileConfig, error) {
+	if len(profiles) == 0 {
+		return MessageProfileConfig{}, fmt.Errorf("no profiles provided")
+	}
+
+	totalWeight := 0
+	for _, p := range profiles {
+		totalWeight += p.Ratio
+	}
+
+	r := rand.IntN(totalWeight)
+	accum := 0
+	for _, p := range profiles {
+		accum += p.Ratio
+		if r < accum {
+			for _, mp := range messageProfiles {
+				if mp.Name == p.MessageProfile {
+					return mp, nil
+				}
+			}
+		}
+	}
+	return MessageProfileConfig{}, fmt.Errorf("no message profile found after weighted random selection")
+}
+
+func ParseMessageRate(messageRate string) (int64, time.Duration) {
+	parts := strings.Split(messageRate, "/")
+	if len(parts) != 2 {
+		return 0, 0
+	}
+	rate, err := strconv.ParseInt(strings.Trim(parts[0], " "), 10, 64)
+	if err != nil {
+		return 0, 0
+	}
+	duration, err := time.ParseDuration(strings.Trim(parts[1], " "))
+	if err != nil {
+		return 0, 0
+	}
+	return rate, duration
 }
