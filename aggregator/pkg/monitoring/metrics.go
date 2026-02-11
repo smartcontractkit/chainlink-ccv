caafd440ae6fdce08079db63c4d201f180136e42
Andres Julia <andres.julia@smartcontract.com>
2026-02-11 09:51:49 -0300

    buckets update

--- aggregator/pkg/monitoring/metrics.go
+++ aggregator/pkg/monitoring/metrics.go
@@ -56,31 +56,31 @@ func MetricViews() []sdkmetric.View {
 		sdkmetric.NewView(
 			sdkmetric.Instrument{Name: "aggregator_time_to_aggregation_seconds"},
 			sdkmetric.Stream{Aggregation: sdkmetric.AggregationExplicitBucketHistogram{
-				Boundaries: []float64{0, 0.01, 0.05, 0.1, 0.5, 1, 5, 10},
+				Boundaries: []float64{0, 0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
 			}},
 		),
 		sdkmetric.NewView(
 			sdkmetric.Instrument{Name: "aggregator_api_request_duration_seconds"},
 			sdkmetric.Stream{Aggregation: sdkmetric.AggregationExplicitBucketHistogram{
-				Boundaries: []float64{0, 0.01, 0.05, 0.1, 0.5, 1, 5, 10},
+				Boundaries: []float64{0, 0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
 			}},
 		),
 		sdkmetric.NewView(
 			sdkmetric.Instrument{Name: "aggregator_get_message_since_number_of_records_returns"},
 			sdkmetric.Stream{Aggregation: sdkmetric.AggregationExplicitBucketHistogram{
-				Boundaries: []float64{0, 1, 5, 10, 50, 100, 500, 1000},
+				Boundaries: []float64{0, 1, 5, 10, 25, 50, 100, 250, 500, 1000},
 			}},
 		),
 		sdkmetric.NewView(
 			sdkmetric.Instrument{Name: "aggregator_storage_duration_seconds"},
 			sdkmetric.Stream{Aggregation: sdkmetric.AggregationExplicitBucketHistogram{
-				Boundaries: []float64{0, 0.01, 0.05, 0.1, 0.5, 1, 5, 10},
+				Boundaries: []float64{0, 0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
 			}},
 		),
 		sdkmetric.NewView(
 			sdkmetric.Instrument{Name: "aggregator_orphan_recovery_duration_seconds"},
 			sdkmetric.Stream{Aggregation: sdkmetric.AggregationExplicitBucketHistogram{
-				Boundaries: []float64{0, 0.1, 0.5, 1, 5, 10, 50, 100},
+				Boundaries: []float64{0.1, 0.5, 1, 2.5, 5, 10, 30, 60, 120, 300},
 			}},
 		),
 	}
