837d518c2f7a1911d2fbe4e4cfffbff3021527ad
Simon B.Robert <simon.robert@smartcontract.com>
2026-02-12 09:37:43 -0500

    Linting issues

--- indexer/pkg/api/handlers/v1/verifier_results.go
+++ indexer/pkg/api/handlers/v1/verifier_results.go
@@ -15,12 +15,12 @@ import (
 )
 
 type VerifierResultsInput struct {
-	SourceChainSelectors []protocol.ChainSelector `doc:"Source chain selectors to filter results by. If empty, results from all source chains will be returned."                                             query:"sourceChainSelectors"`
-	DestChainSelectors   []protocol.ChainSelector `doc:"Destination chain selectors to filter results by. If empty, results from all destination chains will be returned."                                   query:"destChainSelectors"`
-	Start                string                   `doc:"Start time used to filter results. If not provided, results start from the beginning. Accepted formats: RFC3339, unix epoch time (in milliseconds)." form:"start"                 query:"start"`
-	End                  string                   `doc:"End time used to filter results. If not provided, the current server time is used. Accepted formats: RFC3339, unix epoch time (in milliseconds)."    form:"end"                   query:"end"`
-	Limit                uint64                   `doc:"Maximum number of results to return. If not provided, defaults to 100. Maximum allowed is 1000; Requests with a limit greater than 1000 will be rejected with a 400 bad request error."                                                                              form:"limit"                 query:"limit"`
-	Offset               uint64                   `doc:"Number of results to skip before starting to return results. If not provided, defaults to 0."                                                        form:"offset"                query:"offset"`
+	SourceChainSelectors []protocol.ChainSelector `doc:"Source chain selectors to filter results by. If empty, results from all source chains will be returned."                                                                                query:"sourceChainSelectors"`
+	DestChainSelectors   []protocol.ChainSelector `doc:"Destination chain selectors to filter results by. If empty, results from all destination chains will be returned."                                                                      query:"destChainSelectors"`
+	Start                string                   `doc:"Start time used to filter results. If not provided, results start from the beginning. Accepted formats: RFC3339, unix epoch time (in milliseconds)."                                    form:"start"                 query:"start"`
+	End                  string                   `doc:"End time used to filter results. If not provided, the current server time is used. Accepted formats: RFC3339, unix epoch time (in milliseconds)."                                       form:"end"                   query:"end"`
+	Limit                uint64                   `doc:"Maximum number of results to return. If not provided, defaults to 100. Maximum allowed is 1000; Requests with a limit greater than 1000 will be rejected with a 400 bad request error." form:"limit"                 query:"limit"`
+	Offset               uint64                   `doc:"Number of results to skip before starting to return results. If not provided, defaults to 0."                                                                                           form:"offset"                query:"offset"`
 }
 
 type VerifierResultsResponse struct {
