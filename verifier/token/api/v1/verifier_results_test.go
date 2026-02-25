50c37192baed9414a8f49428d126a25f732022a4
Mateusz Sekara <mateusz.sekara@gmail.com>
2026-02-25 10:35:01 +0100

    Minor fixes in api/v1

--- verifier/token/api/v1/verifier_results_test.go
+++ verifier/token/api/v1/verifier_results_test.go
@@ -155,7 +155,7 @@ func Test_VerifierResultsHandler(t *testing.T) {
 		router.ServeHTTP(w, req)
 
 		assert.Equal(t, http.StatusBadRequest, w.Code)
-		assert.JSONEq(t, `{"error":"invalid message_id format: invalid_hex - Bytes32 must start with '0x' prefix: invalid_hex"}`, w.Body.String())
+		assert.JSONEq(t, `{"error":"invalid messageID format"}`, w.Body.String())
 	})
 
 	t.Run("multiple message IDs", func(t *testing.T) {
