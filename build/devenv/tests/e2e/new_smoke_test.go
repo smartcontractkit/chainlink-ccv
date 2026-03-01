c2593db77956e1ab90b05483375f48ac6de40d78
Makram Kamaleddine <makramkd@users.noreply.github.com>
2026-03-01 14:00:02 +0200

    fix logging

--- build/devenv/tests/e2e/new_smoke_test.go
+++ build/devenv/tests/e2e/new_smoke_test.go
@@ -13,24 +13,26 @@ func TestBasic(t *testing.T) {
 	cfg, err := ccv.LoadOutput[ccv.Cfg](GetSmokeTestConfig())
 	require.NoError(t, err)
 
+	ctx := ccv.Plog.WithContext(t.Context())
+
 	harness, err := tcapi.NewTestHarness(
-		t.Context(),
+		ctx,
 		GetSmokeTestConfig(),
 		cfg,
 		chain_selectors.FamilyEVM,
 	)
 	require.NoError(t, err)
 
-	chains, err := harness.Lib.Chains(t.Context())
+	chains, err := harness.Lib.Chains(ctx)
 	require.NoError(t, err)
 	require.GreaterOrEqual(t, len(chains), 2, "expected at least 2 chains for this test in the environment")
 
 	src, dest := chains[0].CCIP17, chains[1].CCIP17
 
 	for _, tc := range tcapi.AllBasicExtraArgsV3(src, dest) {
-		if tc.HavePrerequisites(t.Context(), cfg) {
+		if tc.HavePrerequisites(ctx, cfg) {
 			t.Run(tc.Name(), func(t *testing.T) {
-				require.NoError(t, tc.Run(t.Context(), harness, cfg))
+				require.NoError(t, tc.Run(ctx, harness, cfg))
 			})
 		} else {
 			t.Logf("Skipping %s because current environment does not have the prerequisites", tc.Name())
