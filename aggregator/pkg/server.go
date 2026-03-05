c66e4eacd73ca728ac36137353fbd287fdfa8914
Simon B.Robert <simrob7000@gmail.com>
2026-03-05 09:57:28 -0500

    Return opaque message from gRPC panic recovery instead of panic value (#827)

--- aggregator/pkg/server.go
+++ aggregator/pkg/server.go
@@ -372,7 +372,7 @@ func NewServer(l logger.SugaredLogger, config *model.AggregatorConfig) *Server {
 	aggMonitoring.Metrics().IncrementPendingAggregationsChannelBuffer(context.Background(), config.Aggregation.ChannelBufferSize) // Pre-increment the buffer size metric
 	grpcPanicRecoveryHandler := func(p any) (err error) {
 		l.Error("recovered from panic", "panic", p, "stack", debug.Stack())
-		return status.Errorf(codes.Internal, "%s", p)
+		return status.Error(codes.Internal, "internal server error")
 	}
 
 	grpcOpts := buildGRPCServerOptions(config.Server)
