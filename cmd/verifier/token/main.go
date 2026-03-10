d4931b5c8568a3a4dfad41fa72f3fae908892592
Mateusz Sekara <mateusz.sekara@gmail.com>
2026-03-10 09:43:25 +0100

    Enabling custom log levels

--- cmd/verifier/token/main.go
+++ cmd/verifier/token/main.go
@@ -39,8 +39,16 @@ const (
 )
 
 func main() {
-	// Debug level currently spams a lot of logs from the RPC callers.
-	lggr, err := logger.NewWith(logging.DevelopmentConfig(zapcore.InfoLevel))
+	logLevelStr := os.Getenv("LOG_LEVEL")
+	if logLevelStr == "" {
+		logLevelStr = "info"
+	}
+	var zapLevel zapcore.Level
+	if err := zapLevel.UnmarshalText([]byte(logLevelStr)); err != nil {
+		fmt.Fprintf(os.Stderr, "Invalid LOG_LEVEL '%s', defaulting to 'info'\n", logLevelStr)
+		zapLevel = zapcore.InfoLevel
+	}
+	lggr, err := logger.NewWith(logging.DevelopmentConfig(zapLevel))
 	if err != nil {
 		panic(fmt.Sprintf("Failed to create logger: %v", err))
 	}
