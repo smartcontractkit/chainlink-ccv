711759b4cb6ca9e59dfd505851eafc65ab31bad4
Kyle Martin <kyle.martin@smartcontract.com>
2026-03-16 21:27:30 -0400

    Add prod-testnet datastore

--- build/devenv/cli/send/command.go
+++ build/devenv/cli/send/command.go
@@ -86,7 +86,7 @@ func run(args sendArgs) error {
 	l := zerolog.Ctx(ctx)
 	lib, err := ccv.NewLib(l, envFile, chain_selectors.FamilyEVM)
 	if err != nil {
-		return fmt.Errorf("no implementation found for source chain selector %d", args.srcSel)
+		return fmt.Errorf("failed to initialize environment: %w", err)
 	}
 
 	chains, err := lib.ChainsMap(ctx)
