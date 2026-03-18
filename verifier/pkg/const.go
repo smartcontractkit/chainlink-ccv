1028c9d0077c7748cea6ab7816f794e2bcab8e55
Mateusz Sekara <mateusz.sekara@gmail.com>
2026-03-18 13:04:09 +0100

    Moving coordinator into verifier root package

--- verifier/pkg/const.go
+++ verifier/pkg/const.go
@@ -4,7 +4,6 @@ import vtypes "github.com/smartcontractkit/chainlink-ccv/verifier/pkg/vtypes"
 
 // Constant re-exports - the actual values live in verifier/pkg/vtypes.
 const (
-	DefaultConfigFile               = vtypes.DefaultConfigFile
 	ConfirmationDepth               = vtypes.ConfirmationDepth
 	DefaultJobQueueOperationTimeout = vtypes.DefaultJobQueueOperationTimeout
 	TaskVerifierJobsTableName       = vtypes.TaskVerifierJobsTableName
--- verifier/pkg/vtypes/const.go
+++ verifier/pkg/vtypes/const.go
@@ -3,7 +3,6 @@ package verifier
 import "time"
 
 const (
-	DefaultConfigFile = "/etc/config.toml"
 	// ConfirmationDepth is the number of blocks to wait before considering a block finalized.
 	// This is used for calculating finalized blocks as: (latest - ConfirmationDepth)
 	// when running standalone mode. In CL node it's HeadTracker configuration.
