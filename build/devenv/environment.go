15c6cb4dec1867e0138aff33c319dc8a5a623877
Simon B.Robert <simon.robert@smartcontract.com>
2026-03-23 14:48:45 -0400

    Add topology

--- build/devenv/environment.go
+++ build/devenv/environment.go
@@ -1150,6 +1150,7 @@ func NewEnvironment() (in *Cfg, err error) {
 		instanceName := aggregatorInput.InstanceName()
 		cs := ccipChangesets.GenerateAggregatorConfig(ccipAdapters.GetAggregatorConfigRegistry())
 		output, err := cs.Apply(*e, ccipChangesets.GenerateAggregatorConfigInput{
+			Topology:           topology,
 			ServiceIdentifier:  instanceName + "-aggregator",
 			CommitteeQualifier: aggregatorInput.CommitteeName,
 		})
