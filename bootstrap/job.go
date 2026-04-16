d2509e133fabec145307ce627308fdfd0761e96f
Will Winder <wwinder.unh@gmail.com>
2026-04-16 12:25:32 -0400

    Add more godoc.

--- bootstrap/job.go
+++ bootstrap/job.go
@@ -17,6 +17,7 @@ type JobSpec struct {
 	AppConfig     string `toml:"appConfig"`
 }
 
+// GetGenericConfig decodes the AppConfig field into chainaccess.GenericConfig.
 func (js JobSpec) GetGenericConfig() (chainaccess.GenericConfig, error) {
 	var gcfg chainaccess.GenericConfig
 	if _, err := toml.Decode(js.AppConfig, &gcfg); err != nil {
@@ -25,6 +26,9 @@ func (js JobSpec) GetGenericConfig() (chainaccess.GenericConfig, error) {
 	return gcfg, nil
 }
 
+// GetAppConfig decodes the app config into the provided object. An error is returned
+// if there are any fields aside from blockchain_infos that are left undecoded. See
+// chainaccess.GenericConfig for details about why blockchain_infos is ignored.
 func (js JobSpec) GetAppConfig(cfg any) error {
 	md, err := toml.Decode(js.AppConfig, cfg)
 	if err != nil {
