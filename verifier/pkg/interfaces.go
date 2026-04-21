89639df28d32582c166fac535bb2cca995a9196e
Aleksandr Bukata <96521086+bukata-sa@users.noreply.github.com>
2026-04-21 16:38:50 +0100

    CCIP-9669 finality checker metrics interface (#1024)
    * CCIP-9669 finality checker metrics interface
    
    * Update impl.go

--- verifier/pkg/interfaces.go
+++ verifier/pkg/interfaces.go
@@ -5,10 +5,11 @@ import vtypes "github.com/smartcontractkit/chainlink-ccv/verifier/pkg/vtypes"
 // Type aliases - the actual definitions live in verifier/pkg/vtypes.
 // These aliases preserve backward compatibility for all existing consumers of verifier/pkg.
 type (
-	MessageSigner         = vtypes.MessageSigner
-	VerificationResult    = vtypes.VerificationResult
-	Verifier              = vtypes.Verifier
-	MessageLatencyTracker = vtypes.MessageLatencyTracker
-	Monitoring            = vtypes.Monitoring
-	MetricLabeler         = vtypes.MetricLabeler
+	MessageSigner          = vtypes.MessageSigner
+	VerificationResult     = vtypes.VerificationResult
+	Verifier               = vtypes.Verifier
+	MessageLatencyTracker  = vtypes.MessageLatencyTracker
+	Monitoring             = vtypes.Monitoring
+	MetricLabeler          = vtypes.MetricLabeler
+	FinalityCheckerMetrics = vtypes.FinalityCheckerMetrics
 )
