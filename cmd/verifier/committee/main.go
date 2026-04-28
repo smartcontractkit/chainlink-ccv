619a602a0c894a7bcaadcb679a5678ba5fda756e
Will Winder <wwinder.unh@gmail.com>
2026-04-28 15:22:43 -0400

    lint fix

--- cmd/verifier/committee/main.go
+++ cmd/verifier/committee/main.go
@@ -8,8 +8,8 @@ import (
 	"go.uber.org/zap/zapcore"
 
 	"github.com/smartcontractkit/chainlink-ccv/bootstrap"
-	_ "github.com/smartcontractkit/chainlink-ccv/integration/pkg/accessors/evm" // evm accessor driver
 	cmd "github.com/smartcontractkit/chainlink-ccv/cmd/verifier"
+	_ "github.com/smartcontractkit/chainlink-ccv/integration/pkg/accessors/evm" // evm accessor driver
 )
 
 func main() {
