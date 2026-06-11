65ee8974fc4974e13cc895a0262b6881a36d8a39
Will Winder <wwinder.unh@gmail.com>
2026-06-11 09:59:26 -0400

    Avoid reserved log key.

--- common/logging.go
+++ common/logging.go
@@ -4,5 +4,6 @@ import "github.com/smartcontractkit/chainlink-common/pkg/logger"
 
 // WithService returns lggr with the "service" field set to name.
 func WithService(lggr logger.Logger, name string) logger.Logger {
-	return logger.With(lggr, "service", name)
+	// do not use the reserved "service" key.
+	return logger.With(lggr, "ccip_service", name)
 }
